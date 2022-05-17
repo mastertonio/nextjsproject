196cfaa729df0df0d2168268c336c195b531827e
jason_10xv <jason@10xv.xyz>
2022-05-17 15:51:23 +0800

    login design changes

---
+++ styles/indexStyle.ts
@@ -0,0 +1,66 @@
+import { createStyles } from '@mantine/core';
+
+export const useStyles = createStyles((theme, _params, getRef) => ({
+  wrapper: {
+    // subscribe to color scheme changes right in your styles
+    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[1],
+    maxWidth: 700,
+    width: '100%',
+    height: 450,
+    display: 'flex',
+    flexDirection: 'column',
+    borderRadius: theme.radius.lg,
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    // Dynamic media queries, define breakpoints in theme, use anywhere
+    [`@media (max-width: ${theme.breakpoints.sm}px)`]: {
+      // Type safe child reference in nested selectors via ref
+      [`& .${getRef('child')}`]: {
+        fontSize: theme.fontSizes.xs,
+      },
+    },
+  },
+
+  child: {
+    // assign ref to element
+    ref: getRef('child'),
+    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.white,
+    padding: theme.spacing.md,
+    borderRadius: theme.radius.sm,
+    boxShadow: theme.shadows.md,
+    color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+  },
+
+  login_container: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center'
+  },
+
+  body: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: '95vh',
+      margin: 20,
+      borderRadius: 8
+  },
+
+  button:{
+      height: 44,
+      width: '100%',
+      marginTop: 40,
+  },
+
+
+  title: {
+    marginTop: 30,
+    marginBottom: 40
+  },
+
+  login_footer: {
+    paddingBottom: 20,
+    marginBottom: 20
+  }
+}));
