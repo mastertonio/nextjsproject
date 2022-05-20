ca64a4fe3cf7689507c7cb2c815752ac541579d6
jason-narrasoft <jason.ronda.ns@gmail.com>
2022-05-20 17:57:12 +0800

    Navbar layout

---
+++ app/core/components/navbar/Navbar.tsx
@@ -0,0 +1,47 @@
+import axios from "axios";
+import {
+  Text,
+  useMantineTheme,
+  Burger,
+  Header,
+  MediaQuery,
+} from "@mantine/core";
+import React, { useState } from "react";
+import { useRouter } from "next/router";
+import { useStyles } from "./navStyle"
+import AdminList from "./components/AdminList";
+import PoweredByRoi from "./components/PoweredByRoi";
+import ActionList from "./components/ActionList"
+
+const RoiNavbar: React.FC = () => {
+  const theme = useMantineTheme();
+  const router = useRouter();
+  const { classes } = useStyles();
+  const [opened, setOpened] = useState(false);
+
+  return (
+    <Header
+      height={70}
+      p="md"
+      className={classes.header}
+    >
+      <div style={{ display: "flex", alignItems: "center", height: "100%" }}>
+        <MediaQuery largerThan="sm" styles={{ display: "none" }}>
+          <Burger
+            opened={opened}
+            onClick={() => setOpened((o) => !o)}
+            size="sm"
+            color={theme.colors.gray[5]}
+            mr="xl"
+          />
+        </MediaQuery>
+
+        <AdminList />
+        <PoweredByRoi />
+        <ActionList />
+      </div>
+    </Header>
+  );
+};
+
+export default RoiNavbar;
