3287edd0c09e5503631c26c920599b6bd8da1ef8
jason-narrasoft <jason.ronda.ns@gmail.com>
2022-05-20 18:12:32 +0800

    create new roi modal

---
+++ app/dashboard/components/CreateNewRoi.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import { Modal, Button, Group } from '@mantine/core';
+
+const CreateNewRoi: React.FC = () => {
+  const [opened, setOpened] = useState(false);
+
+  return (
+    <>
+      <Modal
+        opened={opened}
+        onClose={() => setOpened(false)}
+        title="Fill up form here"
+      >
+        {/* Modal content */}
+      </Modal>
+
+      <Group position="center">
+        <Button onClick={() => setOpened(true)}>Create New ROI</Button>
+      </Group>
+    </>
+  );
+}
+
+export default CreateNewRoi
