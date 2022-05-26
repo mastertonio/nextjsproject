df795e518c7751f05048482797d1d5a4693ff71c
jason-narrasoft <jason.ronda.ns@gmail.com>
2022-05-27 04:04:50 +0800

    context wrapper

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { UserContext, UserProvider, useUser} from "../app/context/jwtContext"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <UserProvider>
+      <Component {...pageProps} />
+    </UserProvider>
+  )
 }
 
 export default MyApp
