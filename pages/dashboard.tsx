4373e7e4eff07223814fb33ae23495d2fbe7d11a
jason-narrasoft <jason.ronda.ns@gmail.com>
2022-05-27 12:12:03 +0800

    dashboard with myroi

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { AppShell, useMantineTheme } from "@mantine/core";
+import { AppShell, useMantineTheme, LoadingOverlay, Table, Button, Select } from "@mantine/core";
 import { useStyles } from "../styles/dashboardStyle";
 import axios from "axios";
 
@@ -10,11 +10,15 @@ import DashboardGraph from "../app/dashboard/components/DashboardGraph";
 import ViewCount from "../app/dashboard/components/ViewCount";
 import CreateNewRoi from "../app/dashboard/components/CreateNewRoi";
 import RoiRanking from "../app/dashboard/components/RoiRanking";
+import { useLocalStorage } from "@mantine/hooks";
 
 const Dashboard: React.FC = () => {
   const theme = useMantineTheme();
   const { classes } = useStyles();
   const [data, setData] = useState<any>();
+  const [value] = useLocalStorage({ key: "auth-token" });
+  const [visible, setVisible] = useState(true);
+  const [opened, setOpen] = useState(false);
 
   useEffect(() => {
     const getDashboardData = async () => {
@@ -23,23 +27,58 @@ const Dashboard: React.FC = () => {
           "http://54.159.8.194/v1/dashboard/628247b19e2a1d3a5ef8b9ad",
           {
             headers: {
-              Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2MjgyNDdiMTllMmExZDNhNWVmOGI5YWQiLCJpYXQiOjE2NTM1MDYyMjgsImV4cCI6MTY1MzUwODAyOCwidHlwZSI6ImFjY2VzcyJ9.Px8mAOn1UCguHhV2OioQLslyzoQMesK5GkjIuObTyts`,
+              Authorization: `Bearer ${value}`,
             },
           }
         );
-
-        setData(res?.data)
-        console.log(res.data);
+        if (res) {
+          setData(res?.data);
+          setVisible(false);
+        }
       } catch (error) {
         console.log(error);
       }
     };
 
-    getDashboardData()
-    console.log(data,"datatatata")
-  },[]);
+    getDashboardData();
+  }, []);
 
-  return (
+  const button = <Button onClick={() => setOpen((o) => !o)}>Open</Button>
+
+  const dropdown = ( 
+  <Select
+    placeholder="Active"
+    data={[
+      { value: 'Active', label: 'Active' },
+      { value: 'Inactive', label: 'Inactive' }
+    ]}
+  />
+  )
+
+  const elements = [
+    { button: button, status: dropdown, importance: 4, roiname: 'Name', dates: 'C', views: 4, uniqueViews: 2, actions: '3 buttons' },
+    { button: button, status: dropdown, importance: 4, roiname: 'Name1', dates: 'N', views: 3, uniqueViews: 1, actions: '3 buttons' },
+    { button: button, status: dropdown, importance: 4, roiname: 'Name2', dates: 'Y', views: 1, uniqueViews: 3, actions: '3 buttons' },
+    { button: button, status: dropdown, importance: 4, roiname: 'Name3', dates: 'Ba', views: 3, uniqueViews: 4, actions: '3 buttons' },
+    { button: button, status: dropdown, importance: 4, roiname: 'Name4', dates: 'Ce', views: 3, uniqueViews: 5, actions: '3 buttons' },
+  ];
+
+  const rows = elements.map((element) => (
+    <tr key={element.roiname}>
+      <td>{element.button}</td>
+      <td>{element.status}</td>
+      <td>{element.importance}</td>
+      <td>{element.roiname}</td>
+      <td>{element.dates}</td>
+      <td>{element.views}</td>
+      <td>{element.uniqueViews}</td>
+      <td>{element.actions}</td>
+    </tr>
+  ));
+
+  return visible ? (
+    <LoadingOverlay visible={visible} />
+  ) : (
     <AppShell
       styles={{
         main: {
@@ -53,15 +92,21 @@ const Dashboard: React.FC = () => {
       asideOffsetBreakpoint="sm"
       className={classes.body}
       fixed
-      header={<RoiNavbar admin={data?.admin_list} actions={data?.template_list}/>}
+      header={
+        <RoiNavbar admin={data?.admin_list} actions={data?.template_list} />
+      }
       footer={<RoiFooter />}
     >
       <div className={classes.body}>
         <div className={classes.welcome}>
-          <Welcome name={data?.welcome.account_name} active_roi={data?.welcome.active_roi} current_roi={data?.welcome.current_roi}/>
-          <ViewCount rankings={data?.ranking}/>
+          <Welcome
+            name={data?.welcome.account_name}
+            active_roi={data?.welcome.active_roi}
+            current_roi={data?.welcome.current_roi}
+          />
+          <ViewCount rankings={data?.ranking} />
         </div>
-        <div className={classes.button}>
+        <div className={classes.dashboard_graph}>
           <DashboardGraph chartData={data?.chart} />
         </div>
         <div className={classes.welcome}>
@@ -69,8 +114,22 @@ const Dashboard: React.FC = () => {
           <RoiRanking />
         </div>
       </div>
-      <div>
-        
+      <div className={classes.bar_graph_wrapper}>
+        <Table className={classes.table}>
+          <thead>
+            <tr>
+              <th> </th>
+              <th>Status</th>
+              <th>Importance</th>
+              <th>ROI name</th>
+              <th>Dates</th>
+              <th>Views</th>
+              <th>Unique Views</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>{rows}</tbody>
+        </Table>
       </div>
     </AppShell>
   );
