0c084fe4ff137a80b0721fe9ee9c1935ac3dc5f7
jason-narrasoft <jason.ronda.ns@gmail.com>
2022-06-03 14:43:30 +0800

    add modal form

--- app/dashboard/components/NewRoiModal.tsx
+++ app/dashboard/components/NewRoiModal.tsx
@@ -1,20 +1,54 @@
-import { Modal} from "@mantine/core";
+import {
+  Box,
+  Button,
+  Group,
+  Input,
+  Modal,
+  PasswordInput,
+  TextInput
+} from "@mantine/core";
+import { useForm } from "@mantine/form";
+import { AiOutlineDown } from "react-icons/ai";
 
 interface INewRoiModalProps {
-    opened: boolean,
-    onClose: ()=> void,
-    ModalTitle: string
+  opened: boolean;
+  onClose: () => void;
+  ModalTitle: string;
 }
 
-const CreateNewRoi: React.FC<INewRoiModalProps> = ({ opened, onClose, ModalTitle}) => {
+const CreateNewRoi: React.FC<INewRoiModalProps> = ({
+  opened,
+  onClose,
+  ModalTitle,
+}) => {
+  const form = useForm({
+    initialValues: {
+      name: "",
+      template: [],
+    },
+  });
 
   return (
-    <Modal
-      opened={opened}
-      onClose={onClose}
-      title={ModalTitle}
-    >
-      {/* Modal content */}
+    <Modal opened={opened} onClose={onClose} title={ModalTitle}>
+      <Box sx={{ maxWidth: 340 }} mx="auto">
+        <form onSubmit={form.onSubmit((values) => console.log(values))}>
+          <TextInput
+            required
+            label="Name"
+            placeholder="Enter Name"
+            {...form.getInputProps("name")}
+          />
+
+          <Input component="select" rightSection={<AiOutlineDown />}>
+            <option value="1">1</option>
+            <option value="2">2</option>
+          </Input>
+
+          <Group position="right" mt="md">
+            <Button type="submit">Submit</Button>
+          </Group>
+        </form>
+      </Box>
     </Modal>
   );
 };
