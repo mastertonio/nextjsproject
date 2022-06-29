e3a18130a56843761ab831edfb241fdbc0d31e26
jason-narrasoft <jason.ronda.ns@gmail.com>
2022-06-29 16:53:32 +0800

    HighCharts implementation update

--- app/dashboard/components/DashboardGraph.tsx
+++ app/dashboard/components/DashboardGraph.tsx
@@ -40,67 +40,49 @@ interface IDashboardGraphData {
   chartData: IDashboardData;
 }
 const DashboardGraph: React.FC<IDashboardGraphData> = ({ chartData }) => {
-  const { classes } = useStyles();
-
-  const options = {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: "top" as const,
-      },
-      title: {
-        display: true,
-        text: chartData ? chartData.title.text : "ROIs Created Past Year",
-      },
-    },
-  };
-
   const chartOptions: Highcharts.Options = {
+    chart: {
+      type: "column",
+    },
     title: {
       text: chartData ? chartData.title.text : "ROIs Created Past Year",
+    },
+    xAxis: {
+      categories: chartData
+        ? chartData.xAxis.categories
+        : [
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December",
+          ],
+    },
+    yAxis: {
+      title: { text: "" },
     },
     series: [
       {
         type: "column",
-        data: [1, 2, 3],
-      },
-    ],
-  };
-
-  const labels = chartData
-    ? chartData.xAxis.categories
-    : [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December",
-      ];
-
-  const data = {
-    labels,
-    datasets: [
-      {
-        label: chartData ? chartData.series[0].name : "ROI Created",
+        name: "ROIs Created",
         data: chartData
           ? chartData.series[0].data
           : [
               49.9, 71.5, 106.4, 129.2, 144, 176, 135.6, 148.5, 216.4, 194.1,
               95.6, 54.4,
             ],
-        backgroundColor: "rgba(53, 162, 235, 0.5)"
       },
     ],
   };
 
-  return <Bar options={options} data={data} className={classes.bar} />;
+  return <HighchartsReact highcharts={Highcharts} options={chartOptions} />;
 };
 
 export default DashboardGraph;
