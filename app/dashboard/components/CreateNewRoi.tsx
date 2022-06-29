f881c84f809642c454a3505f4c9afe1c035a53fa
jason-narrasoft <jason.ronda.ns@gmail.com>
2022-06-29 14:05:32 +0800

    new roi button content

--- app/dashboard/components/CreateNewRoi.tsx
+++ app/dashboard/components/CreateNewRoi.tsx
@@ -1,8 +1,17 @@
 import { useState } from "react";
-import { Modal, Button, Group } from "@mantine/core";
+import { Modal, Button, Group, TextInput, Text, Select } from "@mantine/core";
+import { useForm } from "@mantine/form";
+import { AiOutlineDown } from "react-icons/ai";
 
-const CreateNewRoi: React.FC = () => {
+const CreateNewRoi: React.FC<any> = ({ my_roi }) => {
   const [opened, setOpened] = useState(false);
+
+  const form = useForm({
+    initialValues: {
+      name: "",
+      template: [],
+    },
+  });
 
   return (
     <>
@@ -11,11 +20,30 @@ const CreateNewRoi: React.FC = () => {
         onClose={() => setOpened(false)}
         title="Fill up form here"
       >
-        {/* Modal content */}
+        <TextInput
+          required
+          label="Name"
+          placeholder="Enter Name"
+          {...form.getInputProps("name")}
+        />
+
+        <Text>Choose a Template</Text>
+        <Select
+          style={{ width: 150 }}
+          rightSection={<AiOutlineDown />}
+          placeholder="Template"
+          data={
+            my_roi
+              ? my_roi
+              : { value: "No Templates", label: "No Templates Detected" }
+          }
+        />
       </Modal>
 
       <Group position="center">
-        <Button onClick={() => setOpened(true)}>Create New ROI</Button>
+        <Button radius="xs" size="xl" uppercase onClick={() => setOpened(true)}>
+          Create New ROI
+        </Button>
       </Group>
     </>
   );
