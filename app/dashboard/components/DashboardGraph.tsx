108504c4e909169ed2571a4a9b53fc46e56e8850
jason-narrasoft <jason.ronda.ns@gmail.com>
2022-06-29 15:01:13 +0800

    graph changes

--- app/dashboard/components/DashboardGraph.tsx
+++ app/dashboard/components/DashboardGraph.tsx
@@ -9,7 +9,9 @@ import {
   Legend,
 } from "chart.js";
 import { Bar } from "react-chartjs-2";
-import { useStyles } from "../../../styles/dashboardStyle";
+import { useStyles } from "@styles/dashboardStyle";
+import HighchartsReact from "highcharts-react-official";
+import Highcharts from "highcharts";
 
 ChartJS.register(
   CategoryScale,
@@ -53,6 +55,18 @@ const DashboardGraph: React.FC<IDashboardGraphData> = ({ chartData }) => {
     },
   };
 
+  const chartOptions: Highcharts.Options = {
+    title: {
+      text: chartData ? chartData.title.text : "ROIs Created Past Year",
+    },
+    series: [
+      {
+        type: "column",
+        data: [1, 2, 3],
+      },
+    ],
+  };
+
   const labels = chartData
     ? chartData.xAxis.categories
     : ["January", "February", "March", "April", "May", "June", "July"];
@@ -64,7 +78,10 @@ const DashboardGraph: React.FC<IDashboardGraphData> = ({ chartData }) => {
         label: chartData ? chartData.series[0].name : "ROI Created",
         data: chartData
           ? chartData.series[0].data
-          : [400, 450, 550, 450, 650, 600, 730, 300],
+          : [
+              49.9, 71.5, 106.4, 129.2, 144, 176, 135.6, 148.5, 216.4, 194.1,
+              95.6, 54.4,
+            ],
         backgroundColor: "rgba(53, 162, 235, 0.5)",
       },
     ],
