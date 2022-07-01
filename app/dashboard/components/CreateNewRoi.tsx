8e3b512a1c8a20e8cfd822723f587a1aa684171c
jason-narrasoft <jason.ronda.ns@gmail.com>
2022-07-01 17:36:57 +0800

    create new roi changes

--- app/dashboard/components/CreateNewRoi.tsx
+++ app/dashboard/components/CreateNewRoi.tsx
@@ -2,16 +2,45 @@ import { useState } from "react";
 import { Modal, Button, Group, TextInput, Text, Select } from "@mantine/core";
 import { useForm } from "@mantine/form";
 import { AiOutlineDown } from "react-icons/ai";
+import { IAdminListProps } from "@core/components/navbar/components/AdminList";
+import axios from "axios";
+import { useLocalStorage } from "@mantine/hooks";
+import { useRouter } from "next/router";
 
-const CreateNewRoi: React.FC<any> = ({ my_roi }) => {
+const CreateNewRoi: React.FC<IAdminListProps> = ({ actions }) => {
   const [opened, setOpened] = useState(false);
+  const [value] = useLocalStorage({ key: "auth-token" });
+  const [current] = useLocalStorage({ key: "current-user" })
+  const router = useRouter();
+  const p = router.query;
+
+  const actionList = actions?.map((a) => ({
+    key: a.id,
+    value: a.id,
+    label: a.name,
+  }));
 
   const form = useForm({
     initialValues: {
       name: "",
-      template: [],
+      template: "",
     },
   });
+
+  const handleSubmit = async (values: typeof form.values) => {
+    try {
+      console.log(current)
+      await axios.post(
+        `http://54.159.8.194/v1/dashboard/${current}`,
+        { name: values.name, template_id: values.template },
+        { headers: { Authorization: `Bearer ${value}` } }
+      );
+      // router.push("/awdwa");
+    } catch (error) {
+      console.log(error);
+      return error;
+    }
+  };
 
   return (
     <>
@@ -20,24 +49,34 @@ const CreateNewRoi: React.FC<any> = ({ my_roi }) => {
         onClose={() => setOpened(false)}
         title="Fill up form here"
       >
-        <TextInput
-          required
-          label="Name"
-          placeholder="Enter Name"
-          {...form.getInputProps("name")}
-        />
+        <form onSubmit={form.onSubmit(handleSubmit)}>
+          <TextInput
+            required
+            label="Name"
+            placeholder="Enter Name"
+            {...form.getInputProps("name")}
+          />
 
-        <Text>Choose a Template</Text>
-        <Select
-          style={{ width: 150 }}
-          rightSection={<AiOutlineDown />}
-          placeholder="Template"
-          data={
-            my_roi
-              ? my_roi
-              : { value: "No Templates", label: "No Templates Detected" }
-          }
-        />
+          <Text>Choose a Template</Text>
+          <Select
+            style={{ width: 150 }}
+            rightSection={<AiOutlineDown />}
+            placeholder="Template"
+            searchable
+            clearable
+            data={actionList ? actionList : ["No template Detected"]}
+            {...form.getInputProps("template")}
+          />
+
+          <Button
+            type="submit"
+            radius="sm"
+            size="xs"
+            onClick={() => setOpened(false)}
+          >
+            Submit
+          </Button>
+        </form>
       </Modal>
 
       <Group position="center">
