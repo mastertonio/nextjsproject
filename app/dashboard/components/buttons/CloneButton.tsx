62e4b3298e4778b7336fecb6249d86cb3bda0ba1
jason-narrasoft <jason.ronda.ns@gmail.com>
2022-07-01 17:36:27 +0800

    clone button

--- app/dashboard/components/buttons/CloneButton.tsx
+++ app/dashboard/components/buttons/CloneButton.tsx
@@ -1,13 +1,32 @@
 import { useState } from "react";
 import { Button} from "@mantine/core";
 import { FaRegClone } from "react-icons/fa";
+import axios from "axios";
+import { useLocalStorage } from "@mantine/hooks";
+import { useRouter } from "next/router";
+import { IButtonRoiNameProps } from "./EditButton";
 
-const CloneButton: React.FC = () => {
-  const [opened, setOpened] = useState(false);
+const CloneButton: React.FC<IButtonRoiNameProps> = ({ id, name}) => {
+  const [value] = useLocalStorage({ key: "auth-token" });
+  const router = useRouter();
+  const p = router.query;
+  
+  const handleSubmit = async () => {
+    try {
+      await axios.post(
+        `http://54.159.8.194/v1/dashboard/roi/${id}/${p.id}`, { title: name }, { headers: { Authorization: `Bearer ${value}` }}
+      );
+      // router.push("/awdwa");
+    } catch (error) {
+      console.log(error);
+      return error;
+    }
+  };
+
 
   return (
     <>
-        <Button leftIcon={<FaRegClone />} radius="xs" size="xs" color="teal" onClick={() => setOpened(true)}>
+        <Button leftIcon={<FaRegClone />} radius="xs" size="xs" color="teal" onClick={handleSubmit}>
           Clone
         </Button>
     </>
