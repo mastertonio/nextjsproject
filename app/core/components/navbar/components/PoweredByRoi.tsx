3be1367c79fd8f7cdc4f2bd08401f8d4d7996a56
jason-narrasoft <jason.ronda.ns@gmail.com>
2022-07-15 20:46:07 +0800

    powered by change to link

--- app/core/components/navbar/components/PoweredByRoi.tsx
+++ app/core/components/navbar/components/PoweredByRoi.tsx
@@ -3,7 +3,10 @@ import { useStyles } from "@styles/navStyle";
 
 const PoweredByRoi: React.FC = () => {
   const { classes } = useStyles();
-  return <Text>Powered By The ROI Shop</Text>;
+  return (<div className={classes.powered_by}>
+    <Text color="gray" style={{ paddingRight: 5}}>Powered by</Text>    
+    <Text variant="link" component="a" href="https://www.theroishop.com">The ROI Shop</Text>
+  </div>);
 };
 
 export default PoweredByRoi;
