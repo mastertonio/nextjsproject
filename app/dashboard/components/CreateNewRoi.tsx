daf282cfe2ee97d69f8e323efad6b13885eeee98
jason-narrasoft <jason.ronda.ns@gmail.com>
2022-07-15 20:45:16 +0800

    new ROI modal design changes

--- app/dashboard/components/CreateNewRoi.tsx
+++ app/dashboard/components/CreateNewRoi.tsx
@@ -1,16 +1,30 @@
 import { useState } from "react";
-import { Modal, Button, Group, TextInput, Text, Select } from "@mantine/core";
+import {
+  Modal,
+  Button,
+  Group,
+  TextInput,
+  Text,
+  Select,
+  Grid,
+  Checkbox,
+} from "@mantine/core";
 import { useForm } from "@mantine/form";
 import { AiOutlineDown } from "react-icons/ai";
 import { IAdminListProps } from "@core/components/navbar/components/AdminList";
 import axios from "axios";
 import { useLocalStorage } from "@mantine/hooks";
 import { useRouter } from "next/router";
+import { FaPlusSquare } from "react-icons/fa";
 
-const CreateNewRoi: React.FC<IAdminListProps> = ({ actions }) => {
+const CreateNewRoi: React.FC<IAdminListProps> = ({ actions, refetch }) => {
   const [opened, setOpened] = useState(false);
+  const [checked, setChecked] = useState(true);
   const [value] = useLocalStorage({ key: "auth-token" });
-  const [current] = useLocalStorage({ key: "current-user" })
+  const [current] = useLocalStorage({ key: "current-user" });
+  const [templateCreated, setTemplateCreated] = useLocalStorage({
+    key: "cr-temp",
+  });
   const router = useRouter();
   const p = router.query;
 
@@ -29,13 +43,16 @@ const CreateNewRoi: React.FC<IAdminListProps> = ({ actions }) => {
 
   const handleSubmit = async (values: typeof form.values) => {
     try {
-      console.log(current)
-      await axios.post(
+      const res = await axios.post(
         `http://54.159.8.194/v1/dashboard/${current}`,
         { name: values.name, template_id: values.template },
         { headers: { Authorization: `Bearer ${value}` } }
       );
-      // router.push("/awdwa");
+      refetch()
+      if (res && checked) {
+        console.log(res.data.id);
+        router.push(`/templates/${res.data.id}`);
+      }
     } catch (error) {
       console.log(error);
       return error;
@@ -47,41 +64,76 @@ const CreateNewRoi: React.FC<IAdminListProps> = ({ actions }) => {
       <Modal
         opened={opened}
         onClose={() => setOpened(false)}
-        title="Fill up form here"
+        withCloseButton={false}
+        size="50%"
       >
-        <form onSubmit={form.onSubmit(handleSubmit)}>
-          <TextInput
-            required
-            label="Name"
-            placeholder="Enter Name"
-            {...form.getInputProps("name")}
-          />
-
-          <Text>Choose a Template</Text>
-          <Select
-            style={{ width: 150 }}
-            rightSection={<AiOutlineDown />}
-            placeholder="Template"
-            searchable
-            clearable
-            data={actionList ? actionList : ["No template Detected"]}
-            {...form.getInputProps("template")}
-          />
-
-          <Button
-            type="submit"
-            radius="sm"
-            size="xs"
-            onClick={() => setOpened(false)}
+        <form onSubmit={form.onSubmit(handleSubmit)} style={{ margin: 30 }}>
+          <Text
+            weight={700}
+            color="gray"
+            style={{ marginLeft: 20, marginBottom: 40, fontSize: 30 }}
           >
-            Submit
-          </Button>
+            Create A New ROI Calculation
+          </Text>
+
+          <Grid style={{ margin: 20 }}>
+            <Text>Name</Text>
+            <TextInput
+              required
+              style={{ width: 550, marginLeft: "auto" }}
+              placeholder="Enter Name"
+              {...form.getInputProps("name")}
+            />
+          </Grid>
+
+          <Grid style={{ margin: 20, marginBottom: 20 }}>
+            <Text>Choose a Template</Text>
+            <Select
+              style={{ width: 550, marginLeft: "auto" }}
+              rightSection={<AiOutlineDown />}
+              placeholder="Template"
+              searchable
+              clearable
+              data={actionList ? actionList : ["No template Detected"]}
+              {...form.getInputProps("template")}
+            />
+          </Grid>
+          <Grid justify="flex-end" style={{ marginRight: 20, marginBottom: 140}}>
+            <Checkbox checked={checked} onChange={(event) => setChecked(event.currentTarget.checked)} label="Open the Created ROI" />
+          </Grid>
+          <Grid justify="flex-end">
+            <Button
+              type="submit"
+              radius="sm"
+              size="md"
+              style={{ marginRight: 10 }}
+              onClick={() => setOpened(false)}
+            >
+              Create ROI
+            </Button>
+            <Button
+              radius="sm"
+              size="md"
+              onClick={() => setOpened(false)}
+              color="#FFFFFF"
+            >
+              Close
+            </Button>
+          </Grid>
         </form>
       </Modal>
 
       <Group position="center">
-        <Button radius="xs" size="xl" uppercase onClick={() => setOpened(true)}>
-          Create New ROI
+        <Button
+          fullWidth
+          leftIcon={<FaPlusSquare />}
+          radius="md"
+          size="xl"
+          style={{ height: 100, marginRight: 0, fontSize: 25 }}
+          uppercase
+          onClick={() => setOpened(true)}
+        >
+          Create a New ROI
         </Button>
       </Group>
     </>
