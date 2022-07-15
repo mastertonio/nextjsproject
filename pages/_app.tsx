ffbae1b9168b694536a4fff5ab6f38382487c705
jason-narrasoft <jason.ronda.ns@gmail.com>
2022-07-15 20:42:30 +0800

    context setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,21 +6,25 @@ import { Provider } from "react-redux";
 import store from "@redux/store";
 import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
 import { useState } from "react";
+import DashboardContextProvider from "app/context/dashboard.context";
+import UserContextProvider from "app/context/user.context";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <MantineProvider>
         <NotificationsProvider>
-          <QueryClientProvider client={queryClient}>
-            <Hydrate state={pageProps.dehydratedState}>
-              {/* <UserProvider> */}
-              <Component {...pageProps} />
-              {/* </UserProvider> */}
-            </Hydrate>
-          </QueryClientProvider>
+          <UserContextProvider>
+            <DashboardContextProvider>
+              <QueryClientProvider client={queryClient}>
+                <Hydrate state={pageProps.dehydratedState}>
+                  <Component {...pageProps} />
+                </Hydrate>
+              </QueryClientProvider>
+            </DashboardContextProvider>
+          </UserContextProvider>
         </NotificationsProvider>
       </MantineProvider>
     </Provider>
