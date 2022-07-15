8dff7430bdc7d35a7f4e149f2e64bc60cba61f59
jason-narrasoft <jason.ronda.ns@gmail.com>
2022-07-15 20:47:19 +0800

    skeleton loader

---
+++ app/core/components/loader/SkeletonLoader.tsx
@@ -0,0 +1,27 @@
+import { Skeleton } from "@mantine/core";
+
+const skeletonLoader = () => {
+  return (
+    <>
+      <Skeleton height={20} radius="xl" />
+      <Skeleton height={20} mt={12} radius="xl" />
+      <Skeleton height={20} mt={12} radius="xl" />
+      <Skeleton height={20} mt={12} radius="xl" />
+      <Skeleton height={20} mt={12} radius="xl" />
+      <Skeleton height={20} mt={12} radius="xl" />
+      <Skeleton height={20} mt={12} radius="xl" />
+      <Skeleton height={20} mt={12} radius="xl" />
+      <Skeleton height={20} mt={12} radius="xl" />
+      <Skeleton height={20} mt={12} radius="xl" />
+      <Skeleton height={20} mt={12} radius="xl" />
+      <Skeleton height={20} mt={12} radius="xl" />
+      <Skeleton height={20} mt={12} radius="xl" />
+      <Skeleton height={20} mt={12} radius="xl" />
+      <Skeleton height={20} mt={12} radius="xl" />
+      <Skeleton height={20} mt={12} radius="xl" />
+      <Skeleton height={20} mt={12} radius="xl" />
+    </>
+  );
+};
+
+export default skeletonLoader;
