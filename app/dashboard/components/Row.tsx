4a9fb57ac5ac6e4c78252b1770de8819a3ca1925
jason-narrasoft <jason.ronda.ns@gmail.com>
2022-07-28 02:23:05 +0800

    revised table

--- app/dashboard/components/Row.tsx
+++ app/dashboard/components/Row.tsx
@@ -2,14 +2,23 @@ import {
   Button,
   ScrollArea,
   Table,
-  LoadingOverlay,
-  Loader,
   Skeleton,
+  Grid,
+  MultiSelect,
+  Input,
   Pagination,
 } from "@mantine/core";
 import SelectDropdown from "@core/components/dropdown/Select";
-import { Rating } from "react-simple-star-rating";
-import { JSXElementConstructor, Key, ReactElement, ReactFragment, ReactPortal, SetStateAction, useEffect, useState } from "react";
+import {
+  JSXElementConstructor,
+  Key,
+  ReactElement,
+  ReactFragment,
+  ReactPortal,
+  SetStateAction,
+  useEffect,
+  useState,
+} from "react";
 import { useStyles } from "@styles/dashboardStyle";
 import { useRouter } from "next/router";
 import EditButton from "./buttons/EditButton";
@@ -21,10 +30,13 @@ import { useLocalStorage } from "@mantine/hooks";
 import StarRating from "@core/components/rating/Star";
 import { useQuery } from "react-query";
 import Paginate from "./table/paginate";
+import { sortData } from "./table/utils/tableMethods";
+import Th from "./table/Thead";
+import SkeletonLoader from "@core/components/loader/SkeletonLoader";
 
 export interface iDashRowProp {
   id: string;
-  link?: string;
+  link: string;
   importance: number;
   name: string;
   source: string;
@@ -34,32 +46,40 @@ export interface iDashRowProp {
   status: number;
 }
 
+export interface iDashRowProp2 {
+  id: string;
+  link: string;
+  importance: string;
+  name: string;
+  source: string;
+  dateCreated: string;
+  views: string;
+  uniqueViews: string;
+  status: string;
+}
+
+export interface ISearchableDashProp {
+  source: string;
+  name: string;
+  dateCreated: string;
+}
+
 export interface iDashboardRowProps {
   my_roi: iDashRowProp[] | null;
-  filters: string[];
-  search: string;
   refetch: () => void;
 }
 
-const Row: React.FC<iDashboardRowProps> = ({
-  my_roi,
-  filters,
-  search,
-  refetch,
-}) => {
+const Row: React.FC<iDashboardRowProps> = ({ my_roi }) => {
   const [rating, setRating] = useState<number>(0);
   const [star, setStar] = useState<boolean>(true);
   const { classes } = useStyles();
-  const [limit, setLimit] = useState<string>("10")
-  const [activePage, setPage] = useState(1);
   const [value] = useLocalStorage({ key: "auth-token" });
-  const [allRoi, setAllRoi] = useState()
   const router = useRouter();
 
-  const getRoiList = async (limit: number, page: number) => {
+  const getRoiList = async (limitpost: number, page: number) => {
     try {
       const res = await axios.get(
-        `http://54.159.8.194/v1/dashboard/roi/list?limit=${limit}&page=${page}`,
+        `http://54.159.8.194/v1/dashboard/roi/list?limit=${limitpost}&page=${page}`,
         {
           headers: {
             Authorization: `Bearer ${value}`,
@@ -72,102 +92,148 @@ const Row: React.FC<iDashboardRowProps> = ({
     }
   };
 
-  const { isLoading, isError, error, data, isFetching, isPreviousData } =
-    useQuery(["roi_list", activePage], () => getRoiList(10, activePage), {
-      keepPreviousData: true,
-    });
+  const getRoiListAll = async () => {
+    try {
+      const res = await axios.get(`http://54.159.8.194/v1/dashboard/roi/list`, {
+        headers: {
+          Authorization: `Bearer ${value}`,
+        },
+      });
+      return res.data;
+    } catch (error) {
+      return error;
+    }
+  };
 
+  const { isLoading, isError, error, data, refetch, isFetching } = useQuery("get_all_roi", getRoiListAll);
+
+  // const { isLoading, isError, error, data, refetch, isFetching } = useQuery(
+  //   ["roi_list_with_query", activePage],
+  //   () => getRoiList(limit, activePage),
+  //   {
+  //     keepPreviousData: true,
+  //   }
+  // );
+
+  
   useEffect(() => {
-    setAllRoi(data?.results);
-    console.log(allRoi)
-  }, [allRoi, data]);
+    setSortedData(data?.results);
+  }, [data]);
 
-  const myroi = data?.results.map(
-    (item: any) => ({
-      id: item.id,
-      link: item.link,
-      button: (
-        <Button
-          leftIcon={<AiOutlineFolderOpen />}
-          color="blue"
-          onClick={() => {
-            router.push(`/templates/${item.id}`);
-          }}
-        >
-          Open
-        </Button>
-      ),
-      status: (
-        <SelectDropdown
-          cur_status={item.status}
+  const [limit, setLimit] = useState<number>(10);
+  const [activePage, setPage] = useState<number>(1);
+  const [allRoi, setAllRoi] = useState<any>();
+  const [search, setSearch] = useState("");
+  const [filter, setFilter] = useState<string[]>([""]);
+  const [sortedData, setSortedData] = useState(data?.results);
+  const [sortBy, setSortBy] = useState<keyof iDashRowProp | null>(null);
+  const [reverseSortDirection, setReverseSortDirection] = useState(false);
+
+  const indexOfLastPost = activePage * limit;
+  const indexOfFirstPost = indexOfLastPost - limit;
+  const currentPosts = sortedData?.slice(indexOfFirstPost, indexOfLastPost);
+
+  const setSorting = (field: keyof iDashRowProp2) => {
+    const reversed = field === sortBy ? !reverseSortDirection : false;
+    setReverseSortDirection(reversed);
+    setSortBy(field);
+    setSortedData(sortData(data?.results, { sortBy: field, reversed, search }));
+  };
+
+  const handleSearchChange = (event: React.SetStateAction<string>) => {
+     setSearch(event)
+     setSortedData(sortData(data?.results, { sortBy, reversed: reverseSortDirection, search: event }))
+     console.log(sortedData)
+  };
+
+  const myroi = currentPosts?.map((item: any) => ({
+    id: item.id,
+    link: item.link,
+    button: (
+      <Button
+        leftIcon={<AiOutlineFolderOpen />}
+        color="blue"
+        onClick={() => {
+          router.push(`/templates/${item.id}`);
+        }}
+      >
+        Open
+      </Button>
+    ),
+    status: (
+      <SelectDropdown
+        cur_status={item.status}
+        importance={item.importance}
+        rating={rating}
+        id={item.id}
+        refetch={refetch}
+        setStar={setStar}
+        setRating={setRating}
+      />
+    ),
+    importance:
+      item.importance != 0 ? (
+        <StarRating
           importance={item.importance}
-          rating={rating}
           id={item.id}
+          cur_status={item.status}
           refetch={refetch}
-          setStar={setStar}
+          rating={rating}
           setRating={setRating}
+          readOnly={false}
+          setStar={setStar}
+        />
+      ) : (
+        <StarRating
+          importance={item.importance}
+          id={item.id}
+          cur_status={item.status}
+          refetch={refetch}
+          rating={rating}
+          setRating={setRating}
+          readOnly={true}
+          setStar={setStar}
         />
       ),
-      importance:
-      item.importance != 0 ? (
-          <StarRating
-            importance={item.importance}
-            id={item.id}
-            cur_status={item.status}
-            refetch={refetch}
-            rating={rating}
-            setRating={setRating}
-            readOnly={false}
-            setStar={setStar}
-          />
-        ) : (
-          <StarRating
-            importance={item.importance}
-            id={item.id}
-            cur_status={item.status}
-            refetch={refetch}
-            rating={rating}
-            setRating={setRating}
-            readOnly={true}
-            setStar={setStar}
-          />
-        ),
-      roiname: item.name,
-      dates: item.dateCreated,
-      views: item.views,
-      uniqueViews: item.uniqueViews,
-      actions: (
-        <>
-          <EditButton id={item.id} refetch={refetch} name={item.name} />
-          <CloneButton id={item.id} refetch={refetch} name={item.name} />
-          <DeleteButton id={item.id} refetch={refetch} name={item.name} />
-        </>
-      ),
-      source: item.source,
-    })
-  );
+    roiname: item.name,
+    dates: item.dateCreated,
+    views: item.views,
+    uniqueViews: item.uniqueViews,
+    actions: (
+      <>
+        <EditButton id={item.id} refetch={refetch} name={item.name} />
+        <CloneButton id={item.id} refetch={refetch} name={item.name} />
+        <DeleteButton id={item.id} refetch={refetch} name={item.name} />
+      </>
+    ),
+    source: item.source,
+  }));
 
-  // const filterSearch = {
-  //   nodes: ,
-  // };
-
-  // const rows = filterSearch.nodes?.map((element) => (
-  //   <tr key={element.id}>
-  //     <td>{element.button}</td>
-  //     <td>{element.status}</td>
-  //     <td>{element.importance}</td>
-  //     <td>{element.roiname}</td>
-  //     <td>{element.dates}</td>
-  //     <td>{element.views}</td>
-  //     <td>{element.uniqueViews}</td>
-  //     <td>{element.actions}</td>
-  //   </tr>
-  // ));
-
-  // if(fetching) return (<div>Fetching data...</div>)
 
   return (
     <div>
+      <Grid style={{ margin: 20 }}>
+        <MultiSelect
+          style={{ width: 150 }}
+          placeholder="Filter"
+          searchable
+          clearable
+          data={ []}
+          onChange={(event) => {
+            setFilter(event);
+          }}
+        />
+        <Input
+          variant="default"
+          placeholder="Search for ROI"
+          style={{ marginLeft: "auto" }}
+          onChange={(event: {
+            target: { value: React.SetStateAction<string> };
+          }) => {
+            handleSearchChange(event.target.value);
+          }}
+        />
+      </Grid>
       <Table
         className={classes.table}
         horizontalSpacing="xl"
@@ -178,75 +244,164 @@ const Row: React.FC<iDashboardRowProps> = ({
           <tr>
             <th style={{ width: 110 }}></th>
             <th style={{ width: 170 }}>Status</th>
-            <th style={{ width: 140 }}>Importance</th>
-            <th style={{ width: 420 }}>ROI name</th>
-            <th style={{ width: 270 }}>Dates</th>
-            <th style={{ width: 170 }}>Views</th>
-            <th>Unique Views</th>
+            <th style={{ width: 120 }}>Importance</th>
+            <Th
+              sorted={sortBy === "name"}
+              reversed={reverseSortDirection}
+              onSort={() => setSorting("name")}
+              style={{ width: 420 }}
+            >
+              ROI name
+            </Th>
+            <Th
+              sorted={sortBy === "dateCreated"}
+              reversed={reverseSortDirection}
+              onSort={() => setSorting("dateCreated")}
+              style={{ width: 270 }}
+            >
+              Dates
+            </Th>
+            <Th
+              sorted={sortBy === "views"}
+              reversed={reverseSortDirection}
+              onSort={() => setSorting("views")}
+              style={{ width: 170 }}
+            >
+              Views
+            </Th>
+            <Th
+              sorted={sortBy === "uniqueViews"}
+              reversed={reverseSortDirection}
+              onSort={() => setSorting("uniqueViews")}
+              style={{ width: 220 }}
+            >
+              Unique Views
+            </Th>
             <th></th>
           </tr>
         </thead>
       </Table>
       <ScrollArea style={{ height: 580 }}>
         <Table className={classes.table} highlightOnHover verticalSpacing="xs">
-          {isLoading ? (
-            <>
-              <Skeleton height={20} radius="xl" />
-              <Skeleton height={20} mt={12} radius="xl" />
-              <Skeleton height={20} mt={12} radius="xl" />
-              <Skeleton height={20} mt={12} radius="xl" />
-              <Skeleton height={20} mt={12} radius="xl" />
-              <Skeleton height={20} mt={12} radius="xl" />
-              <Skeleton height={20} mt={12} radius="xl" />
-              <Skeleton height={20} mt={12} radius="xl" />
-              <Skeleton height={20} mt={12} radius="xl" />
-              <Skeleton height={20} mt={12} radius="xl" />
-              <Skeleton height={20} mt={12} radius="xl" />
-              <Skeleton height={20} mt={12} radius="xl" />
-              <Skeleton height={20} mt={12} radius="xl" />
-              <Skeleton height={20} mt={12} radius="xl" />
-              <Skeleton height={20} mt={12} radius="xl" />
-              <Skeleton height={20} mt={12} radius="xl" />
-              <Skeleton height={20} mt={12} radius="xl" />
-            </>
-          ) : (
+          {isLoading ? ( <SkeletonLoader />) : (
             <tbody>
-              { myroi.filter((item: { roiname: string | string[]; }) => item.roiname.includes(search))
-                    .map((element: { id: Key | null | undefined; button: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | ReactFragment | ReactPortal | null | undefined; status: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | ReactFragment | ReactPortal | null | undefined; importance: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | ReactFragment | ReactPortal | null | undefined; roiname: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | ReactFragment | ReactPortal | null | undefined; dates: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | ReactFragment | ReactPortal | null | undefined; views: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | ReactFragment | ReactPortal | null | undefined; uniqueViews: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | ReactFragment | ReactPortal | null | undefined; actions: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | ReactFragment | ReactPortal | null | undefined; }) => (
-                      <tr key={element.id} style={{ height: 20 }}>
-                        <td style={{ width: 10 }}>{element.button}</td>
-                        <td style={{ width: 180 }}>{element.status}</td>
-                        <td style={{ width: 140 }}>{element.importance}</td>
-                        <td
-                          style={{ cursor: "pointer", width: 420 }}
-                          onClick={() => {
-                            router.push(`/templates/${element.id}`);
-                          }}
-                        >
-                          {element.roiname}
-                        </td>
-                        <td style={{ width: 270 }}>{element.dates}</td>
-                        <td style={{ width: 170 }}>{element.views} views</td>
-                        <td style={{ width: 190 }}>
-                          {element.uniqueViews} unique views
-                        </td>
-                        <td
-                          style={{
-                            display: "flex",
-                            justifyContent: "flex-end",
-                            height: 57,
-                          }}
-                        >
-                          {element.actions}
-                        </td>
-                      </tr>
-                    ))}
+              {myroi?.map(
+                  (element: {
+                    id: Key | null | undefined;
+                    button:
+                      | string
+                      | number
+                      | boolean
+                      | ReactElement<any, string | JSXElementConstructor<any>>
+                      | ReactFragment
+                      | ReactPortal
+                      | null
+                      | undefined;
+                    status:
+                      | string
+                      | number
+                      | boolean
+                      | ReactElement<any, string | JSXElementConstructor<any>>
+                      | ReactFragment
+                      | ReactPortal
+                      | null
+                      | undefined;
+                    importance:
+                      | string
+                      | number
+                      | boolean
+                      | ReactElement<any, string | JSXElementConstructor<any>>
+                      | ReactFragment
+                      | ReactPortal
+                      | null
+                      | undefined;
+                    roiname:
+                      | string
+                      | number
+                      | boolean
+                      | ReactElement<any, string | JSXElementConstructor<any>>
+                      | ReactFragment
+                      | ReactPortal
+                      | null
+                      | undefined;
+                    dates:
+                      | string
+                      | number
+                      | boolean
+                      | ReactElement<any, string | JSXElementConstructor<any>>
+                      | ReactFragment
+                      | ReactPortal
+                      | null
+                      | undefined;
+                    views:
+                      | string
+                      | number
+                      | boolean
+                      | ReactElement<any, string | JSXElementConstructor<any>>
+                      | ReactFragment
+                      | ReactPortal
+                      | null
+                      | undefined;
+                    uniqueViews:
+                      | string
+                      | number
+                      | boolean
+                      | ReactElement<any, string | JSXElementConstructor<any>>
+                      | ReactFragment
+                      | ReactPortal
+                      | null
+                      | undefined;
+                    actions:
+                      | string
+                      | number
+                      | boolean
+                      | ReactElement<any, string | JSXElementConstructor<any>>
+                      | ReactFragment
+                      | ReactPortal
+                      | null
+                      | undefined;
+                  }) => (
+                    <tr key={element.id} style={{ height: 20 }}>
+                      <td style={{ width: 10 }}>{element.button}</td>
+                      <td style={{ width: 180 }}>{element.status}</td>
+                      <td style={{ width: 140 }}>{element.importance}</td>
+                      <td
+                        style={{ cursor: "pointer", width: 420 }}
+                        onClick={() => {
+                          router.push(`/templates/${element.id}`);
+                        }}
+                      >
+                        {element.roiname}
+                      </td>
+                      <td style={{ width: 270 }}>{element.dates}</td>
+                      <td style={{ width: 170 }}>{element.views}</td>
+                      <td style={{ width: 190 }}>{element.uniqueViews}</td>
+                      <td
+                        style={{
+                          display: "flex",
+                          justifyContent: "flex-end",
+                          height: 57,
+                        }}
+                      >
+                        {element.actions}
+                      </td>
+                    </tr>
+                  )
+                )}
             </tbody>
           )}
         </Table>
       </ScrollArea>
       <div>
-        <Paginate page={isLoading ? 10 : data.totalPages} limit={limit} totalResults={data?.totalResults} setLimit={setLimit} activePage={activePage} setPage={setPage} />
+        <Paginate
+          refetch={refetch}
+          page={sortedData ? Math.ceil(sortedData?.length / limit) : 10}
+          limit={limit}
+          totalResults={sortedData?.length}
+          setLimit={setLimit}
+          activePage={activePage}
+          setPage={setPage}
+        />
       </div>
     </div>
   );
