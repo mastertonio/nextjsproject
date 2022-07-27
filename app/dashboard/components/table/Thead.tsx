e29c54a97cd1412cbae4a4ba024f0adfe41dfb9a
jason-narrasoft <jason.ronda.ns@gmail.com>
2022-07-28 02:24:00 +0800

    Reusable table head component

---
+++ app/dashboard/components/table/Thead.tsx
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import {
+  createStyles,
+  Table,
+  ScrollArea,
+  UnstyledButton,
+  Group,
+  Text,
+  Center,
+  TextInput,
+} from '@mantine/core';
+import { useStyles } from '@styles/tableStyle';
+import { IconSelector, IconChevronDown, IconChevronUp, IconSearch } from '@tabler/icons';
+
+interface ThProps {
+    children: React.ReactNode;
+    reversed: boolean;
+    sorted: boolean;
+    onSort(): void;
+    style?: object
+  }
+
+const Th: React.FC<ThProps> = ({ children, reversed, sorted, onSort, style }) => {
+    const { classes } = useStyles();
+    const Icon = sorted ? (reversed ? IconChevronUp : IconChevronDown) : IconSelector;
+    return (
+      <th style={style}>
+        <UnstyledButton onClick={onSort} className={classes.control}>
+          <Group position="apart">
+            <Text weight={500} size="sm">
+              {children}
+            </Text>
+            <Center className={classes.icon}>
+              <Icon size={14} stroke={1.5} />
+            </Center>
+          </Group>
+        </UnstyledButton>
+      </th>
+    );
+  }
+  
+
+  export default Th
