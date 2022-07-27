b45b4a8fcd7e7812ac9a86c7b17b950b0474d343
jason-narrasoft <jason.ronda.ns@gmail.com>
2022-07-28 02:22:10 +0800

    react modern drawer

--- app/core/components/navbar/Navbar.tsx
+++ app/core/components/navbar/Navbar.tsx
@@ -1,45 +1,69 @@
 import axios from "axios";
 import {
-  Text,
   useMantineTheme,
   Burger,
   Header,
   MediaQuery,
+  Group,
+  Button,
 } from "@mantine/core";
 import React, { useState } from "react";
 import { useRouter } from "next/router";
-import { useStyles } from "@styles/navStyle"
+import { useStyles } from "@styles/navStyle";
 import AdminList, { IAdminListProps } from "./components/AdminList";
 import PoweredByRoi from "./components/PoweredByRoi";
-import ActionList from "./components/ActionList"
+import ActionList from "./components/ActionList";
+import Drawer from 'react-modern-drawer'
+import 'react-modern-drawer/dist/index.css'
+import DashboardDrawer from "../drawer/DrawerContent";
 
-const RoiNavbar: React.FC<IAdminListProps> = ({ admin, actions }) => {
+const RoiNavbar: React.FC = () => {
   const theme = useMantineTheme();
   const router = useRouter();
   const { classes } = useStyles();
   const [opened, setOpened] = useState(false);
+  const [isOpen, setIsOpen] = React.useState(false)
+  const toggleDrawer = () => {
+      setIsOpen((prevState) => !prevState)
+  }
 
   return (
-    <Header
-      height={70}
-      p="md"
-      className={classes.header}
-    >
-      <div style={{ display: "flex", alignItems: "center", height: "100%" }}>
-        <MediaQuery largerThan="sm" styles={{ display: "none" }}>
-          <Burger
-            opened={opened}
-            onClick={() => setOpened((o) => !o)}
-            size="sm"
-            color={theme.colors.gray[5]}
-            mr="xl"
-          />
-        </MediaQuery>
-
-        <AdminList admin={admin} />
+    <Header height={70} p="md" className={classes.header}>
+      <MediaQuery largerThan="sm" styles={{ display: "none" }}>
+        <Burger
+          opened={opened}
+          onClick={() => setOpened((o) => !o)}
+          size="sm"
+          color={theme.colors.gray[5]}
+          mr="xl"
+        />
+      </MediaQuery>
+      {router.route.includes("dashboard") ? (
+        <div>
+            <Drawer
+                open={isOpen}
+                onClose={toggleDrawer}
+                direction='left'
+                style={{ backgroundColor: '#2f4050', padding: 20}}
+            >
+                <DashboardDrawer />
+            </Drawer>
+          <Button
+            style={{ marginRight: "auto", backgroundColor: '#00acac' }}
+            onClick={toggleDrawer}
+          >
+            Navigate
+          </Button>
+        </div>
+        
+      ) : (
+        ""
+      )}
+      <Group position="right" style={{ marginLeft: 'auto'}}>
+        <AdminList />
         <PoweredByRoi />
-        <ActionList actions={actions}/>
-      </div>
+        <ActionList />
+      </Group>
     </Header>
   );
 };
