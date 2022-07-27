855e7f2716e0425a1c86d4466923fd9f6559b2f1
jason-narrasoft <jason.ronda.ns@gmail.com>
2022-07-28 02:23:48 +0800

    paginate changes

--- app/dashboard/components/table/paginate.tsx
+++ app/dashboard/components/table/paginate.tsx
@@ -1,36 +1,75 @@
 import { Pagination, Select, Text } from "@mantine/core";
-
+import React, { HtmlHTMLAttributes, useState } from "react";
+import { usePagination } from '@mantine/hooks';
 interface IPaginateProps {
-  page: number,
-  setLimit: (num: string) => void
-  totalResults: number,
-  limit: string,
-  activePage: number,
-  setPage: (num: number) => void
+  page: number;
+  setLimit: (num: number) => void;
+  totalResults: number;
+  limit: number;
+  activePage: number;
+  setPage: (num: number) => void;
+  refetch: () => void;
 }
 
-const Paginate: React.FC<IPaginateProps> = ({ page, setLimit, limit, totalResults, activePage, setPage}) => {
-  const defaultlimit = "10"
+const Paginate: React.FC<IPaginateProps> = ({
+  page,
+  setLimit,
+  limit,
+  totalResults,
+  activePage,
+  setPage,
+  refetch,
+}) => {
+  const [defaultlimit, setDefaultLimit] = useState<string>(`${limit}`);
+  // const pagination = usePagination({ total: page, initialPage: activePage });
+
   return (
-    <div style={{ display: "flex", flexDirection: "row", alignItems: "center", margin: 20}}>
-      <Text style={{ marginRight: 5}}>Showing {limit} of {totalResults} ROIs </Text>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        margin: 20,
+      }}
+    >
+      <Text style={{ marginRight: 5 }}>
+        Showing {} of {} ROIs{" "}
+      </Text>
       <Select
-       style={{ width: 70}}
+        style={{ width: 70 }}
         placeholder="Pick one"
-        defaultValue={defaultlimit}
+        value={defaultlimit}
         data={[
           { value: "10", label: "10" },
           { value: "20", label: "20" },
           { value: "30", label: "30" },
           { value: "50", label: "50" },
-          { value: "100", label: "100" }
+          { value: "100", label: "100" },
         ]}
-        onChange={(value)=> {
-          setLimit(value || defaultlimit)
+        onChange={(value) => {
+          setDefaultLimit(value || defaultlimit);
+          switch (value) {
+            case "20":
+              return setLimit(20);
+            case "30":
+              return setLimit(30);
+            case "50":
+              return setLimit(50);
+            case "100":
+              return setLimit(100);
+            default:
+              return setLimit(10);
+          }
         }}
       />
-      <Text style={{ marginLeft: 5}}> ROIs per page </Text>
-      <Pagination style={{ marginLeft: "auto"}} total={page ? page : 10} color="teal" onChange={setPage}/>
+      <Text style={{ marginLeft: 5 }}> ROIs per page </Text>
+      <Pagination
+        style={{ marginLeft: "auto" }}
+        total={page}
+        color="teal"
+        page={activePage}
+        onChange={setPage}
+      />
     </div>
   );
 };
