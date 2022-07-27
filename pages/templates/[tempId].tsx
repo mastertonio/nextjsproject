7dce5db9175a34fb60e20964320b312c8758edaa
jason-narrasoft <jason.ronda.ns@gmail.com>
2022-07-28 02:26:47 +0800

    revised templates

--- pages/templates/[tempId].tsx
+++ pages/templates/[tempId].tsx
@@ -1,74 +1,98 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
   AppShell,
+  Button,
+  Group,
+  LoadingOverlay,
   Navbar,
-  Header,
-  Footer,
-  Aside,
+  PasswordInput,
+  Stack,
   Text,
-  MediaQuery,
-  Burger,
+  TextInput,
   useMantineTheme,
-} from '@mantine/core';
-import RoiFooter from '@core/components/footer/Footer';
-import { useRouter } from 'next/router';
-import { selectUser } from '@redux/reducers/user/userSlice';
-import { useAppSelector } from '@redux/store';
+} from "@mantine/core";
+import RoiFooter from "@core/components/footer/Footer";
+import { useRouter } from "next/router";
+import RoiNavbar from "@core/components/navbar/Navbar";
+import Sidebar from "@core/components/sidebar/Sidebar";
+import { useLocalStorage } from "@mantine/hooks";
+import axios from "axios";
+import { useQuery } from "react-query";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
 
 const Templates: React.FC = () => {
   const theme = useMantineTheme();
   const [opened, setOpened] = useState(false);
-  const router = useRouter()
-  const p = router.query
-  const user = useAppSelector(selectUser);
+  const router = useRouter();
+  const [user, setUser] = useState<any>({});
+  const p = router.query;
+
+  const { register, handleSubmit } = useForm({
+    mode: "onBlur",
+    defaultValues: {
+      first_name: "",
+      last_name: "",
+      avatar: "",
+      email: "",
+      phone_number: "",
+    },
+  });
+
+  const onSubmit = (data: any) => {
+    alert(data.first_name);
+  };
+
+  const [value] = useLocalStorage({ key: "auth-token" });
+  const [current, setCurrent] = useLocalStorage({ key: "current-user" });
+
+  const getCurrentUser = async () => {
+    try {
+      const res = await axios.get(`http://54.159.8.194/v1/users/${current}`, {
+        headers: {
+          Authorization: `Bearer ${value}`,
+        },
+      });
+      return res.data;
+    } catch (error) {
+      return error;
+    }
+  };
+
+  const { isLoading, status, data, isFetching, refetch } = useQuery(
+    "userList",
+    getCurrentUser
+  );
+
+  useEffect(() => {
+    setUser(data);
+  }, [data]);
+
+  if (isLoading)
+    return <LoadingOverlay visible={router.isReady && isLoading} />;
 
   return (
     <AppShell
       styles={{
         main: {
-          background: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0],
+          background:
+            theme.colorScheme === "dark"
+              ? theme.colors.dark[8]
+              : theme.colors.gray[0],
         },
       }}
       navbarOffsetBreakpoint="sm"
       asideOffsetBreakpoint="sm"
       fixed
-      navbar={
-        <Navbar p="md" hiddenBreakpoint="sm" hidden={!opened} width={{ sm: 200, lg: 300 }}>
-          <Text>Application navbar</Text>
-        </Navbar>
-      }
-      aside={
-        <MediaQuery smallerThan="sm" styles={{ display: 'none' }}>
-          <Aside p="md" hiddenBreakpoint="sm" width={{ sm: 200, lg: 300 }}>
-            <Text>Application sidebar</Text>
-          </Aside>
-        </MediaQuery>
-      }
-      footer={
-        <RoiFooter />
-      }
-      header={
-        <Header height={70} p="md">
-          <div style={{ display: 'flex', alignItems: 'center', height: '100%' }}>
-            <MediaQuery largerThan="sm" styles={{ display: 'none' }}>
-              <Burger
-                opened={opened}
-                onClick={() => setOpened((o) => !o)}
-                size="sm"
-                color={theme.colors.gray[6]}
-                mr="xl"
-              />
-            </MediaQuery>
-
-            <Text>Application header</Text>
-          </div>
-        </Header>
-      }
+      navbar={<Sidebar name={user?.name} user={user} />}
+      // footer={
+      //   <RoiFooter />
+      // }
+      header={<RoiNavbar />}
     >
-      <Text>Welcome! your template id is {p.id}</Text>
+      Welcome {user?.name} to your Templates Page
     </AppShell>
   );
-}
+};
 
-
-export default Templates
+export default Templates;
