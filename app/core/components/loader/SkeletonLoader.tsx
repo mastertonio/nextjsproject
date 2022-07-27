a6486b85c2a3839052c947398a801e3453b94e86
jason-narrasoft <jason.ronda.ns@gmail.com>
2022-07-28 02:20:11 +0800

    loader for tables

--- app/core/components/loader/SkeletonLoader.tsx
+++ app/core/components/loader/SkeletonLoader.tsx
@@ -1,27 +1,27 @@
 import { Skeleton } from "@mantine/core";
 
-const skeletonLoader = () => {
+const SkeletonLoader: React.FC = () => {
   return (
     <>
-      <Skeleton height={20} radius="xl" />
-      <Skeleton height={20} mt={12} radius="xl" />
-      <Skeleton height={20} mt={12} radius="xl" />
-      <Skeleton height={20} mt={12} radius="xl" />
-      <Skeleton height={20} mt={12} radius="xl" />
-      <Skeleton height={20} mt={12} radius="xl" />
-      <Skeleton height={20} mt={12} radius="xl" />
-      <Skeleton height={20} mt={12} radius="xl" />
-      <Skeleton height={20} mt={12} radius="xl" />
-      <Skeleton height={20} mt={12} radius="xl" />
-      <Skeleton height={20} mt={12} radius="xl" />
-      <Skeleton height={20} mt={12} radius="xl" />
-      <Skeleton height={20} mt={12} radius="xl" />
-      <Skeleton height={20} mt={12} radius="xl" />
-      <Skeleton height={20} mt={12} radius="xl" />
-      <Skeleton height={20} mt={12} radius="xl" />
-      <Skeleton height={20} mt={12} radius="xl" />
-    </>
+    <Skeleton height={20} radius="xl" />
+    <Skeleton height={20} mt={12} radius="xl" />
+    <Skeleton height={20} mt={12} radius="xl" />
+    <Skeleton height={20} mt={12} radius="xl" />
+    <Skeleton height={20} mt={12} radius="xl" />
+    <Skeleton height={20} mt={12} radius="xl" />
+    <Skeleton height={20} mt={12} radius="xl" />
+    <Skeleton height={20} mt={12} radius="xl" />
+    <Skeleton height={20} mt={12} radius="xl" />
+    <Skeleton height={20} mt={12} radius="xl" />
+    <Skeleton height={20} mt={12} radius="xl" />
+    <Skeleton height={20} mt={12} radius="xl" />
+    <Skeleton height={20} mt={12} radius="xl" />
+    <Skeleton height={20} mt={12} radius="xl" />
+    <Skeleton height={20} mt={12} radius="xl" />
+    <Skeleton height={20} mt={12} radius="xl" />
+    <Skeleton height={20} mt={12} radius="xl" />
+  </>
   );
 };
 
-export default skeletonLoader;
+export default SkeletonLoader;
