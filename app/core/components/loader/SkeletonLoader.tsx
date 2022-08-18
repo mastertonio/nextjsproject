69f48ff6a0e010da59b47023cf37b243e0aa5d27
jason-narrasoft <jason.ronda.ns@gmail.com>
2022-08-18 11:45:59 +0800

    skeleton loader changes

--- app/core/components/loader/SkeletonLoader.tsx
+++ app/core/components/loader/SkeletonLoader.tsx
@@ -5,55 +5,113 @@ const SkeletonLoader: React.FC = () => {
     <tbody>
         <tr>
           <td><Skeleton height={20} radius="xl" /></td>
-        </tr>
-        <tr>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
           <td><Skeleton height={20} mt={12} radius="xl" /></td>
         </tr>
         <tr>
-        <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
         </tr>
         <tr>
-        <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
         </tr>
         <tr>
-        <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
         </tr>
         <tr>
-        <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
         </tr>
         <tr>
-        <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
         </tr>
         <tr>
-        <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
         </tr>
         <tr>
-        <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
         </tr>
         <tr>
-        <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
         </tr>
         <tr>
-        <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
         </tr>
         <tr>
-        <td><Skeleton height={20} mt={12} radius="xl" /></td>
-        </tr>
-        {/* <tr>
-        <td><Skeleton height={20} mt={12} radius="xl" /></td>
-        </tr>
-        <tr>
-        <td><Skeleton height={20} mt={12} radius="xl" /></td>
-        </tr>
-        <tr>
-        <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
         </tr>
         <tr>
-        <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
+          <td><Skeleton height={20} mt={12} radius="xl" /></td>
         </tr>
-        <tr>
-        <td><Skeleton height={20} mt={12} radius="xl" /></td>
-        </tr> */}
+        
     </tbody>
   );
 };
