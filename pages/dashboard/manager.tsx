2096ea2711defc4d4c9324c7809db3d3c9e018ee
jason-narrasoft <jason.ronda.ns@gmail.com>
2022-08-27 00:33:14 +0800

    manager dashboard

---
+++ pages/dashboard/manager.tsx
@@ -0,0 +1,274 @@
+import React, { SetStateAction, useEffect, useState } from "react";
+import {
+  AppShell,
+  useMantineTheme,
+  LoadingOverlay,
+  Select,
+  Text,
+  Input,
+  Grid,
+  MultiSelect,
+  Button,
+  ScrollArea,
+  Table,
+  Center,
+  Badge,
+} from "@mantine/core";
+import { useStyles } from "@styles/dashboardStyle";
+import axios from "axios";
+import { useQuery } from "react-query";
+import {
+  GetServerSideProps,
+  GetServerSidePropsContext,
+  InferGetServerSidePropsType,
+  GetStaticPaths,
+  GetStaticPathsContext,
+} from "next";
+
+import RoiNavbar from "@core/components/navbar/Navbar";
+import RoiFooter from "@core/components/footer/Footer";
+import Welcome from "@dashboard/components/Welcome";
+import DashboardGraph from "@dashboard/components/DashboardGraph";
+import ViewCount from "@dashboard/components/ViewCount";
+import CreateNewRoi from "@dashboard/components/CreateNewRoi";
+import RoiRanking from "@dashboard/components/RoiRanking";
+import { useLocalStorage } from "@mantine/hooks";
+import Row from "@dashboard/components/Row";
+import { useRouter } from "next/router";
+import { FaPlusSquare } from "react-icons/fa";
+import Paginate from "@app/dashboard/components/table/paginate";
+import EditCompanyButton from "@app/company/components/buttons/EditCompanyButton";
+import { FiUsers } from "react-icons/fi";
+import { HiTemplate } from "react-icons/hi";
+import Segmented from "@app/core/components/buttons/Segmented";
+import { AiOutlineFolderOpen } from "react-icons/ai";
+import {
+  filterCompanyUsersData,
+  ICompanyUsersProps,
+  sortCompanyUsersData,
+} from "@app/dashboard/components/table/utils/tableMethods";
+import Th from "@app/dashboard/components/table/Thead";
+import SkeletonLoader from "@app/core/components/loader/SkeletonLoader";
+import { ICompanyElement } from "pages/company";
+import Sidebar from "@app/core/components/sidebar/Sidebar";
+import Pophover from "@app/core/components/popover/Pophover";
+import { useAppDispatch, useAppSelector } from "@redux/store";
+import EditCompanyUserButton from "@app/company/components/buttons/EditCompanyUser";
+import AddCompanyUserButton from "@app/company/components/buttons/AddCompanyUser";
+import TransferButton from "@app/company/components/buttons/Transfer";
+import CompanyUserTable from "@app/company/user/table";
+import MainLoader from "@app/core/components/loader/MainLoader";
+import ManagerDashboardGraph from "@app/dashboard/components/ManagerDashboardGraph";
+
+interface ICompanyUsersElements {
+  id: React.Key | null | undefined;
+  username:
+    | string
+    | number
+    | boolean
+    | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+    | React.ReactFragment
+    | React.ReactPortal
+    | null
+    | undefined;
+  created_rois:
+    | string
+    | number
+    | boolean
+    | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+    | React.ReactFragment
+    | React.ReactPortal
+    | null
+    | undefined;
+  role:
+    | string
+    | number
+    | boolean
+    | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+    | React.ReactFragment
+    | React.ReactPortal
+    | null
+    | undefined;
+  manager_email:
+    | string
+    | number
+    | boolean
+    | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+    | React.ReactFragment
+    | React.ReactPortal
+    | null
+    | undefined;
+  currency:
+    | string
+    | number
+    | boolean
+    | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+    | React.ReactFragment
+    | React.ReactPortal
+    | null
+    | undefined;
+  status:
+    | string
+    | number
+    | boolean
+    | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+    | React.ReactFragment
+    | React.ReactPortal
+    | null
+    | undefined;
+  actions:
+    | string
+    | number
+    | boolean
+    | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+    | React.ReactFragment
+    | React.ReactPortal
+    | null
+    | undefined;
+}
+
+const ManagerDashboard: React.FC = () => {
+  const router = useRouter();
+  const theme = useMantineTheme();
+  const { classes, cx } = useStyles();
+  const [value] = useLocalStorage({ key: "auth-token" });
+  const [current, setCurrent] = useLocalStorage({ key: "current-user" });
+  const [company, setCompany] = useLocalStorage({ key: "my-company" });
+  const users = useAppSelector((state) => state.user);
+  const p = router.query;
+
+  const getCompanyUsers = async () => {
+    try {
+      const res = await axios.get(
+        `http://54.159.8.194/v1/company/${company}/user`,
+        {
+          headers: {
+            Authorization: `Bearer ${value}`,
+          },
+        }
+      );
+      return res.data;
+    } catch (error) {
+      router.push("/forbidden");
+      return error;
+    }
+  };
+
+  const { isLoading, isError, error, data, refetch, isFetching } = useQuery(
+    "get_all_company_users",
+    getCompanyUsers
+  );
+
+  const [limit, setLimit] = useState<number>(10);
+  const [activePage, setPage] = useState<number>(1);
+  const [allRoi, setAllRoi] = useState<any>();
+  const [search, setSearch] = useState("");
+  const [filter, setFilter] = useState<string[]>([]);
+  const [sortedData, setSortedData] = useState(data);
+  const [sortBy, setSortBy] = useState<keyof ICompanyUsersProps | null>(null);
+  const [reverseSortDirection, setReverseSortDirection] = useState(false);
+  const [status, setStatus] = useState("");
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    console.log(filter);
+  }, [filter]);
+
+  useEffect(() => {
+    setSortedData(data);
+  }, [data]);
+
+  const indexOfLastPost = activePage * limit;
+  const indexOfFirstPost = indexOfLastPost - limit;
+  const currentPosts = sortedData?.slice(indexOfFirstPost, indexOfLastPost);
+
+  const setSorting = (field: keyof ICompanyUsersProps) => {
+    const reversed = field === sortBy ? !reverseSortDirection : false;
+    setReverseSortDirection(reversed);
+    setSortBy(field);
+    setSortedData(
+      sortCompanyUsersData(data, { sortBy: field, reversed, search })
+    );
+  };
+
+  const handleSearchChange = (event: React.SetStateAction<string>) => {
+    setSearch(event);
+    setSortedData(
+      sortCompanyUsersData(data, {
+        sortBy,
+        reversed: reverseSortDirection,
+        search: event,
+      })
+    );
+  };
+
+  const handleFilterChange = (event: SetStateAction<string[]>) => {
+    setFilter(event);
+    setSortedData(
+      sortCompanyUsersData(data, {
+        sortBy,
+        reversed: reverseSortDirection,
+        search: event,
+      })
+    );
+  };
+
+  const companies = currentPosts?.map((item: ICompanyUsersProps) => ({
+    id: item._id,
+    username: <Pophover title={item.email} />,
+    first_name: item.first_name,
+    last_name: item.last_name,
+    created_rois: item.created_rois,
+    currency: item.currency,
+    status: item.status,
+    role: item.role,
+    manager_email: item.manager_email,
+    manager_id: item.manager_id,
+    actions: (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "flex-end",
+          alignItems: "center",
+        }}
+      >
+        <EditCompanyUserButton
+          id={item._id}
+          myCompany={item}
+          refetch={refetch}
+          name={item.email}
+        />
+        <TransferButton id={item._id} name={item.email} refetch={refetch} />
+      </div>
+    ),
+  }));
+
+  return isLoading ? (
+    <MainLoader />
+  ) : (
+    <AppShell
+      styles={{
+        main: {
+          background:
+            theme.colorScheme === "dark"
+              ? theme.colors.dark[8]
+              : theme.colors.gray[0],
+        },
+      }}
+      navbarOffsetBreakpoint="sm"
+      asideOffsetBreakpoint="sm"
+      className=""
+      fixed
+      header={<RoiNavbar />}
+    >
+      <div style={{ margin: 10, backgroundColor: "white", padding: 50 }}>
+        <ManagerDashboardGraph />
+      </div>
+      <div style={{ margin: 10, backgroundColor: "white", padding: 90 }}>
+        <CompanyUserTable update={refetch} />
+      </div>
+    </AppShell>
+  );
+};
+
+export default ManagerDashboard;
