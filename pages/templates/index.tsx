43cbf3225c5f4fe1790abe4f6fab9630b3237dbe
jason-narrasoft <jason.ronda.ns@gmail.com>
2022-08-27 00:32:20 +0800

    templates page

--- pages/templates/index.tsx
+++ pages/templates/index.tsx
@@ -1,53 +1,180 @@
-import React, { useState } from "react";
+import React, { SetStateAction, useEffect, useState } from "react";
 import {
   AppShell,
-  LoadingOverlay,
   useMantineTheme,
+  Input,
+  Grid,
+  Button,
+  ScrollArea,
+  Table,
+  Badge,
+  Divider,
 } from "@mantine/core";
-import { useRouter } from "next/router";
-import RoiNavbar from "@core/components/navbar/Navbar";
-import Sidebar from "@core/components/sidebar/Sidebar";
-import { useLocalStorage } from "@mantine/hooks";
+import { useStyles } from "@styles/dashboardStyle";
 import axios from "axios";
 import { useQuery } from "react-query";
-import { useEffect } from "react";
+import {
+  GetServerSideProps,
+  GetServerSidePropsContext,
+  InferGetServerSidePropsType,
+  GetStaticPaths,
+  GetStaticPathsContext,
+} from "next";
 
-const Templates: React.FC = () => {
-  const theme = useMantineTheme();
-  const [opened, setOpened] = useState(false);
+import RoiNavbar from "@core/components/navbar/Navbar";
+import { useLocalStorage } from "@mantine/hooks";
+import { useRouter } from "next/router";
+import Paginate from "@app/dashboard/components/table/paginate";
+import {
+  sortCompanyTemplatesData,
+  ICompanyTemplatesProps,
+} from "@app/dashboard/components/table/utils/tableMethods";
+import Th from "@app/dashboard/components/table/Thead";
+import SkeletonLoader from "@app/core/components/loader/SkeletonLoader";
+import { ICompanyElement } from "pages/company";
+import Sidebar from "@app/core/components/sidebar/Sidebar";
+import Pophover from "@app/core/components/popover/Pophover";
+import { useAppDispatch, useAppSelector } from "@redux/store";
+import EditCompanyUserButton from "@app/company/components/buttons/EditCompanyUser";
+import AddCompanyUserButton from "@app/company/components/buttons/AddCompanyUser";
+import TransferButton from "@app/company/components/buttons/Transfer";
+import CompanyUserTable from "@app/company/user/table";
+import MainLoader from "@app/core/components/loader/MainLoader";
+import shortUUID from "short-uuid";
+import AddTemplateButton from "@app/company/components/buttons/AddTemplate";
+import EditTemplateButton from "@app/company/components/buttons/EditTemplate";
+import TemplateVersion from "@app/company/components/TemplateVersion";
+
+const TemplatesDashboard: React.FC = () => {
   const router = useRouter();
-  const [user, setUser] = useState<any>({});
-  const p = router.query;
-
+  const theme = useMantineTheme();
+  const { classes, cx } = useStyles();
   const [value] = useLocalStorage({ key: "auth-token" });
   const [current, setCurrent] = useLocalStorage({ key: "current-user" });
+  const [company, setCompany] = useLocalStorage({ key: "my-company" });
+  const users = useAppSelector((state) => state.user);
+  const p = router.query;
 
-  const getCurrentUser = async () => {
+  const getCompanyTemplates = async () => {
     try {
-      const res = await axios.get(`http://54.159.8.194/v1/users/${current}`, {
-        headers: {
-          Authorization: `Bearer ${value}`,
-        },
-      });
+      const res = await axios.get(
+        `http://54.159.8.194/v1/company/${company}/template`,
+        {
+          headers: {
+            Authorization: `Bearer ${value}`,
+          },
+        }
+      );
       return res.data;
     } catch (error) {
       return error;
     }
   };
 
-  const { isLoading, status, data, isFetching, refetch } = useQuery(
-    "userList",
-    getCurrentUser
+  const { isLoading, isError, error, data, refetch, isFetching } = useQuery(
+    "get_all_company_templates",
+    getCompanyTemplates
   );
 
+  const [limit, setLimit] = useState<number>(10);
+  const [activePage, setPage] = useState<number>(1);
+  const [allRoi, setAllRoi] = useState<any>();
+  const [search, setSearch] = useState("");
+  const [filter, setFilter] = useState<string[]>([]);
+  const [sortedData, setSortedData] = useState(data);
+  const [sortBy, setSortBy] = useState<keyof ICompanyTemplatesProps | null>(
+    null
+  );
+  const [reverseSortDirection, setReverseSortDirection] = useState(false);
+  const [name, setName] = useState("");
+  const [scrolled, setScrolled] = useState(false);
+
   useEffect(() => {
-    setUser(data);
+    setSortedData(data);
   }, [data]);
 
-  if (isLoading)
-    return <LoadingOverlay visible={router.isReady && isLoading} />;
+  const indexOfLastPost = activePage * limit;
+  const indexOfFirstPost = indexOfLastPost - limit;
+  const currentPosts = sortedData?.slice(indexOfFirstPost, indexOfLastPost);
 
-  return (
+  const setSorting = (field: keyof ICompanyTemplatesProps) => {
+    const reversed = field === sortBy ? !reverseSortDirection : false;
+    setReverseSortDirection(reversed);
+    setSortBy(field);
+    setSortedData(
+      sortCompanyTemplatesData(data, { sortBy: field, reversed, search })
+    );
+  };
+
+  const handleSearchChange = (event: React.SetStateAction<string>) => {
+    setSearch(event);
+    setSortedData(
+      sortCompanyTemplatesData(data, {
+        sortBy,
+        reversed: reverseSortDirection,
+        search: event,
+      })
+    );
+  };
+
+  const handleFilterChange = (event: SetStateAction<string[]>) => {
+    setFilter(event);
+    setSortedData(
+      sortCompanyTemplatesData(data, {
+        sortBy,
+        reversed: reverseSortDirection,
+        search: event,
+      })
+    );
+  };
+
+  const [temp_id, setTemp] = useState<string>('')
+  const [comp_id, setComp] = useState<string>('')
+  const templates = currentPosts?.map((item: ICompanyTemplatesProps) => ({
+    id: item._id,
+    name: (
+      <span
+        style={{ cursor: "pointer", width: 10 }}
+        onClick={() => {
+          setTemp(item._id)
+          setComp(item.company_id)
+          setName(item.name)
+          refetch
+        }}
+      >
+        {item.name}
+      </span>
+    ),
+    notes: item.notes,
+    status: item.status,
+    active: item.active,
+    company_id: item.company_id,
+    actions: (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "flex-end",
+          alignItems: "center",
+        }}
+      >
+        <EditTemplateButton
+          id={item._id}
+          notes={item.notes}
+          status={item.active}
+          refetch={refetch}
+          name={item.name}
+          key={shortUUID.generate()}
+        />
+        <Button radius="sm" size="xs" color="red" style={{ marginLeft: 1 }}>
+          Delete
+        </Button>
+      </div>
+    ),
+  }));
+
+  return isLoading ? (
+    <MainLoader />
+  ) : (
     <AppShell
       styles={{
         main: {
@@ -59,16 +186,181 @@ const Templates: React.FC = () => {
       }}
       navbarOffsetBreakpoint="sm"
       asideOffsetBreakpoint="sm"
+      className=""
       fixed
-      navbar={<Sidebar />}
-      // footer={
-      //   <RoiFooter />
-      // }
       header={<RoiNavbar />}
+      navbar={<Sidebar />}
     >
-      Welcome {user?.name} to your Templates Index Page
+      <div style={{ margin: 10, backgroundColor: "white", padding: 50 }}>
+        <Grid style={{ margin: 20 }}>
+          {/* <TempList filter={filter} handleFilter={handleFilterChange} /> */}
+          <AddTemplateButton refetch={refetch} />
+          <Input
+            variant="default"
+            placeholder="Search for ROI"
+            style={{ marginLeft: "auto" }}
+            onChange={(event: {
+              target: { value: React.SetStateAction<string> };
+            }) => {
+              handleSearchChange(event.target.value);
+            }}
+          />
+        </Grid>
+        <ScrollArea
+          style={{ height: 590 }}
+          onScrollPositionChange={({ y }) => setScrolled(y !== 0)}
+        >
+          <Table
+            className={classes.table}
+            highlightOnHover
+            verticalSpacing="xs"
+            horizontalSpacing="xl"
+          >
+            <thead
+              className={cx(classes.header, { [classes.scrolled]: scrolled })}
+              style={{ zIndex: 50 }}
+            >
+              <tr>
+                <Th
+                  sorted={sortBy === "name"}
+                  reversed={reverseSortDirection}
+                  onSort={() => setSorting("name")}
+                  style={{ width: 300 }}
+                >
+                  Template Name
+                </Th>
+                <Th
+                  sorted={sortBy === "notes"}
+                  reversed={reverseSortDirection}
+                  onSort={() => setSorting("notes")}
+                  style={{ width: 670 }}
+                >
+                  Notes
+                </Th>
+                <Th
+                  sorted={sortBy === "status"}
+                  reversed={reverseSortDirection}
+                  onSort={() => setSorting("status")}
+                  style={{ width: 130 }}
+                >
+                  Status
+                </Th>
+                <th></th>
+              </tr>
+            </thead>
+            {isLoading ? (
+              <SkeletonLoader />
+            ) : (
+              <tbody>
+                {templates?.map(
+                  (element: {
+                    _id: React.Key | null | undefined;
+                    name:
+                      | string
+                      | number
+                      | boolean
+                      | React.ReactElement<
+                          any,
+                          string | React.JSXElementConstructor<any>
+                        >
+                      | React.ReactFragment
+                      | React.ReactPortal
+                      | null
+                      | undefined;
+                    notes:
+                      | string
+                      | number
+                      | boolean
+                      | React.ReactElement<
+                          any,
+                          string | React.JSXElementConstructor<any>
+                        >
+                      | React.ReactFragment
+                      | React.ReactPortal
+                      | null
+                      | undefined;
+                    status:
+                      | string
+                      | number
+                      | boolean
+                      | React.ReactElement<
+                          any,
+                          string | React.JSXElementConstructor<any>
+                        >
+                      | React.ReactFragment
+                      | React.ReactPortal
+                      | null
+                      | undefined;
+                    actions:
+                      | string
+                      | number
+                      | boolean
+                      | React.ReactElement<
+                          any,
+                          string | React.JSXElementConstructor<any>
+                        >
+                      | React.ReactFragment
+                      | React.ReactPortal
+                      | null
+                      | undefined;
+                  }) => (
+                    <tr key={element._id} style={{ height: 20 }}>
+                      <td>{element.name}</td>
+                      <td
+                        style={{
+                          cursor: "pointer",
+                          width: 140,
+                          paddingLeft: 30,
+                        }}
+                      >
+                        {element.notes}
+                      </td>
+                      <td
+                        style={{
+                          width: 110,
+                        }}
+                      >
+                        <Badge color="green" variant="outline">
+                          {element.status}
+                        </Badge>
+                      </td>
+                      <td>{element.actions}</td>
+                    </tr>
+                  )
+                )}
+              </tbody>
+            )}
+          </Table>
+        </ScrollArea>
+        <div>
+          <Paginate
+            refetch={refetch}
+            page={sortedData ? Math.ceil(sortedData?.length / limit) : 10}
+            limit={limit}
+            totalResults={sortedData?.length}
+            setLimit={setLimit}
+            activePage={activePage}
+            setPage={setPage}
+            firstIndex={indexOfFirstPost}
+            lastIndex={indexOfLastPost}
+          />
+        </div>
+        <Divider my="lg" size="xl" style={{ marginTop: 70}}/>
+        
+      <TemplateVersion update={refetch} comp_id={comp_id}  temp_id={temp_id} first_temp={data?.[0]._id} name={name} />
+      </div>
     </AppShell>
   );
 };
 
-export default Templates;
+// export async function getServerSideProps(ctx: any) {
+//   // Fetch data from external API
+//   console.log(ctx.req.cookies)
+//   const res = await fetch(`https://jsonplaceholder.typicode.com/posts/`)
+//   const data = await res.json()
+
+//   // Pass data to the page via props
+//   return { props: { data } }
+// }
+
+export default TemplatesDashboard;
