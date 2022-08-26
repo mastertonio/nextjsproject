a1421bdc19dcc0dfa947394d97fd9814a69e9a86
jason-narrasoft <jason.ronda.ns@gmail.com>
2022-08-27 00:37:40 +0800

    add template button

---
+++ app/company/components/buttons/AddTemplate.tsx
@@ -0,0 +1,231 @@
+import { useEffect, useState } from "react";
+import {
+  Modal,
+  Button,
+  Text,
+  TextInput,
+  Grid,
+  Stack,
+  Group,
+  FileButton,
+  Textarea,
+  NumberInput,
+  PasswordInput,
+  Select,
+  MultiSelect,
+} from "@mantine/core";
+import { useForm } from "@mantine/form";
+import { AiFillPlusCircle, AiOutlineEdit } from "react-icons/ai";
+import axios from "axios";
+import { useLocalStorage } from "@mantine/hooks";
+import { useRouter } from "next/router";
+import { showNotification, updateNotification } from "@mantine/notifications";
+import { IconCheck } from "@tabler/icons";
+import { ICompanyProps } from "@app/dashboard/components/table/utils/tableMethods";
+import { DatePicker } from "@mantine/dates";
+import dayjs from "dayjs";
+import { useQuery } from "react-query";
+
+export interface IButtonAddCompanyProps {
+  refetch: () => void;
+}
+
+const AddTemplateButton: React.FC<IButtonAddCompanyProps> = ({
+  refetch,
+}) => {
+  const [opened, setOpened] = useState(false);
+  const [value] = useLocalStorage({ key: "auth-token" });
+  const [current, setCurrent] = useLocalStorage({ key: "current-user" });
+  const [company, setCompany] = useLocalStorage({ key: "my-company" });
+  const router = useRouter();
+  const p = router.query;
+  const [file, setFile] = useState<File | null>(null);
+  const [startDate, setStartDate] = useState<Date | null>(new Date());
+  const [endDate, setEndDate] = useState<Date | null>(new Date());
+  const [state, setState] = useState();
+  const [filter, setFilter] = useState<string[]>([""]);
+
+
+  const form = useForm({
+    initialValues: {
+      name: '',
+      status: '',
+      notes: ''
+    },
+  });
+
+  const handleSubmit = async (values: typeof form.values) => {
+    try {
+      showNotification({
+        id: "edit-comp",
+        loading: true,
+        title: `Updating`,
+        message: "Please wait, updating edited row",
+        autoClose: false,
+        disallowClose: true,
+        color: "teal",
+      });
+      const response = await axios.post(
+        `http://54.159.8.194/v1/company/${company}/template`,
+        {
+          name: values.name,
+          status: values.status,
+          notes: values.notes,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${value}`,
+          },
+        }
+      );
+      if (response) {
+        refetch();
+        updateNotification({
+          id: "edit-comp",
+          color: "teal",
+          title: `row was updated to `,
+          message: "A row was updated! and renamed",
+          icon: <IconCheck size={16} />,
+          autoClose: 2500,
+        });
+        form.reset();
+        setFile(null);
+        setStartDate(null);
+        setEndDate(null);
+      }
+    } catch (error) {
+      updateNotification({
+        id: "edit-comp",
+        color: "red",
+        title: "Updating a table row failed",
+        message: "Something went wrong, Please try again",
+        autoClose: false,
+      });
+      return error;
+    }
+  };
+
+  return (
+    <>
+      <Modal
+        opened={opened}
+        onClose={() => setOpened(false)}
+        withCloseButton={false}
+        size="xl"
+        padding={0}
+      >
+        <Text
+          weight={700}
+          color="gray"
+          style={{
+            padding: 30,
+            fontSize: 30,
+            backgroundColor: "#073e52",
+            color: "white",
+          }}
+          align="center"
+        >
+          Add Company Template
+        </Text>
+
+        <form onSubmit={form.onSubmit(handleSubmit)}>
+          <Stack
+            justify="flex-start"
+            sx={(theme) => ({
+              backgroundColor:
+                theme.colorScheme === "dark"
+                  ? theme.colors.dark[8]
+                  : theme.colors.gray[0],
+              height: 300,
+            })}
+          >
+            <Grid
+              style={{
+                marginLeft: 30,
+                marginRight: 30,
+                marginTop: 30,
+                marginBottom: 15,
+              }}
+            >
+              <Text>Name: </Text>
+              <TextInput
+                required
+                style={{ width: 550, marginLeft: "auto" }}
+                placeholder="Enter Template Name"
+                {...form.getInputProps("name")}
+              />
+            </Grid>
+            <Grid
+              style={{
+                marginLeft: 30,
+                marginRight: 30,
+                marginBottom: 15,
+              }}
+            >
+              <Text>Notes: </Text>
+              <Textarea
+                required
+                style={{ width: 550, marginLeft: "auto" }}
+                placeholder="Enter Notes for Template"
+                {...form.getInputProps("notes")}
+              />
+            </Grid>
+            <Grid
+              style={{
+                marginLeft: 30,
+                marginRight: 30,
+                marginBottom: 15,
+              }}
+            >
+              <Text>Status: </Text>
+              <TextInput
+                required
+                style={{ width: 550, marginLeft: "auto" }}
+                placeholder="Enter User Last Name"
+                {...form.getInputProps("status")}
+              />
+            </Grid>
+          </Stack>
+
+          <Grid justify="flex-end" style={{ margin: 20 }}>
+            <Button
+              type="submit"
+              radius="sm"
+              size="sm"
+              color="teal"
+              style={{ marginRight: 10 }}
+              onClick={() => setOpened(false)}
+            >
+              Save Template
+            </Button>
+            <Button
+              radius="sm"
+              size="sm"
+              onClick={() => setOpened(false)}
+              style={{
+                backgroundColor: "white",
+                color: "black",
+                borderColor: "gray",
+              }}
+            >
+              Close
+            </Button>
+          </Grid>
+        </form>
+      </Modal>
+
+      <Button
+        leftIcon={<AiFillPlusCircle />}
+        radius="sm"
+        size="sm"
+        onClick={() => setOpened(true)}
+        color="teal"
+        style={{ width: 300 }}
+      >
+        Add Template
+      </Button>
+    </>
+  );
+};
+
+export default AddTemplateButton;
