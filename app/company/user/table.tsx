6db7c0c292c55794243adf0200d748a17a77d7f5
jason-narrasoft <jason.ronda.ns@gmail.com>
2022-09-09 21:11:20 +0800

    user table optimization

--- app/company/user/table.tsx
+++ app/company/user/table.tsx
@@ -60,6 +60,8 @@ import AddCompanyUserButton from "@app/company/components/buttons/AddCompanyUser
 import TransferButton from "@app/company/components/buttons/Transfer";
 import TransferSingleButton from "../components/buttons/TransferSingle";
 import shortUUID from "short-uuid";
+import MainLoader from "@app/core/components/loader/MainLoader";
+import FourOhFour from "pages/404";
 
 export interface ICompanyUserTable {
   user_id: string;
@@ -73,39 +75,39 @@ export interface ICompanyUserTable {
 }
 
 export interface ICompanyUserTableProps {
-  update: () => void
+  update: () => void;
+  company: string;
 }
 
-const CompanyUserTable: React.FC<ICompanyUserTableProps> = ({ update }) => {
-  const router = useRouter();
-  const theme = useMantineTheme();
+const getCompanyUsers = async (company: string, value: string) => {
+  try {
+    const res = await axios.get(
+      `http://54.159.8.194/v1/company/${company}/user/templates`,
+      {
+        headers: {
+          Authorization: `Bearer ${value}`,
+        },
+      }
+    );
+    return res.data;
+  } catch (error) {
+    return error;
+  }
+};
+
+const CompanyUserTable: React.FC<ICompanyUserTableProps> = ({
+  company,
+  update,
+}) => {
   const { classes, cx } = useStyles();
   const [value] = useLocalStorage({ key: "auth-token" });
-  const [current, setCurrent] = useLocalStorage({ key: "current-user" });
-  const [company, setCompany] = useLocalStorage({ key: "my-company" });
-  const users = useAppSelector((state) => state.user);
-  const p = router.query;
 
-  const getCompanyUsers = async () => {
-    try {
-      const res = await axios.get(
-        `http://54.159.8.194/v1/company/${company}/user/templates`,
-        {
-          headers: {
-            Authorization: `Bearer ${value}`,
-          },
-        }
-      );
-      return res.data;
-    } catch (error) {
-      return error;
-    }
-  };
-
-  const { isLoading, isError, error, data, refetch, isFetching } = useQuery(
-    "get_all_company_users_with_templates",
-    getCompanyUsers
-  );
+  const { isLoading, isError, error, data, refetch, isFetching, isSuccess } =
+    useQuery(
+      ["get_all_company_users_with_templates"],
+      () => getCompanyUsers(company, value),
+      { enabled: company.length > 0 }
+    );
 
   const [limit, setLimit] = useState<number>(10);
   const [activePage, setPage] = useState<number>(1);
@@ -113,14 +115,12 @@ const CompanyUserTable: React.FC<ICompanyUserTableProps> = ({ update }) => {
   const [search, setSearch] = useState("");
   const [filter, setFilter] = useState<string[]>([]);
   const [sortedData, setSortedData] = useState(data);
-  const [sortBy, setSortBy] = useState<keyof ICompanyTemplateUsersProps | null>(null);
+  const [sortBy, setSortBy] = useState<keyof ICompanyTemplateUsersProps | null>(
+    null
+  );
   const [reverseSortDirection, setReverseSortDirection] = useState(false);
   const [status, setStatus] = useState("");
   const [scrolled, setScrolled] = useState(false);
-
-  useEffect(() => {
-    console.log(filter);
-  }, [filter]);
 
   useEffect(() => {
     setSortedData(data);
@@ -180,145 +180,162 @@ const CompanyUserTable: React.FC<ICompanyUserTableProps> = ({ update }) => {
             alignItems: "center",
           }}
         >
-          <TransferSingleButton id={item.user_id} name={item.username} refetch={update} tempId={item.template_id}/>
+          <TransferSingleButton
+            id={item.user_id}
+            name={item.username}
+            refetch={update}
+            tempId={item.template_id}
+          />
         </div>
       ),
     })
   );
 
-  return (
-    <div>
-      <div style={{ margin: 10, backgroundColor: "white", padding: 50 }}>
-        <Grid style={{ margin: 20 }}>
-          <Text weight={700}> The ROI Shop User Calculators</Text>
-          <Input
-            variant="default"
-            placeholder="Search for ROI"
-            style={{ marginLeft: "auto" }}
-            onChange={(event: {
-              target: { value: React.SetStateAction<string> };
-            }) => {
-              handleSearchChange(event.target.value);
-            }}
-          />
-        </Grid>
-        <ScrollArea
-          style={{ height: 590 }}
-          onScrollPositionChange={({ y }) => setScrolled(y !== 0)}
-        >
-          <Table
-            className={classes.table}
-            highlightOnHover
-            verticalSpacing="xs"
-            horizontalSpacing="xl"
+  if (isFetching) {
+    return <MainLoader />;
+  }
+
+  if (isSuccess) {
+    return (
+      <div>
+        <div style={{ margin: 10, backgroundColor: "white", padding: 50 }}>
+          <Grid style={{ margin: 20 }}>
+            <Text weight={700}> The ROI Shop User Calculators</Text>
+            <Input
+              variant="default"
+              placeholder="Search for ROI"
+              style={{ marginLeft: "auto" }}
+              onChange={(event: {
+                target: { value: React.SetStateAction<string> };
+              }) => {
+                handleSearchChange(event.target.value);
+              }}
+            />
+          </Grid>
+          <ScrollArea
+            style={{ height: 590 }}
+            onScrollPositionChange={({ y }) => setScrolled(y !== 0)}
           >
-            <thead
-              className={cx(classes.header, { [classes.scrolled]: scrolled })}
-              style={{ zIndex: 50 }}
+            <Table
+              className={classes.table}
+              highlightOnHover
+              verticalSpacing="xs"
+              horizontalSpacing="xl"
             >
-              <tr>
-                <Th
-                  sorted={sortBy === "username"}
-                  reversed={reverseSortDirection}
-                  onSort={() => setSorting("username")}
-                  style={{ width: 300 }}
-                >
-                  User Name
-                </Th>
-                <Th
-                  sorted={sortBy === "calculator_name"}
-                  reversed={reverseSortDirection}
-                  onSort={() => setSorting("calculator_name")}
-                  style={{ width: 250 }}
-                >
-                  Calculator Name
-                </Th>
-                <Th
-                  sorted={sortBy === "template_name"}
-                  reversed={reverseSortDirection}
-                  onSort={() => setSorting("template_name")}
-                  style={{ width: 250 }}
-                >
-                  Template Name
-                </Th>
-                <Th
-                  sorted={sortBy === "createdAt"}
-                  reversed={reverseSortDirection}
-                  onSort={() => setSorting("createdAt")}
-                  style={{ width: 250 }}
-                >
-                  Created Date
-                </Th>
-                <Th
-                  sorted={sortBy === "visits"}
-                  reversed={reverseSortDirection}
-                  onSort={() => setSorting("visits")}
-                  style={{ width: 110 }}
-                >
-                  Visits
-                </Th>
-                <Th
-                  sorted={sortBy === "unique_ip"}
-                  reversed={reverseSortDirection}
-                  onSort={() => setSorting("unique_ip")}
-                  style={{ width: 130 }}
-                >
-                  Unique
-                </Th>
-                <th></th>
-              </tr>
-            </thead>
-            {isLoading ? (
-              <SkeletonLoader />
-            ) : (
-              <tbody>
-                {companyUsers?.map((element: any) => (
-                  <tr key={shortUUID.generate()} style={{ height: 20 }}>
-                    <td style={{ width: 10 }}>{element.username}</td>
-                    <td
-                      style={{
-                        cursor: "pointer",
-                        width: 140,
-                        paddingLeft: 30,
-                      }}
-                    >
-                      {element.calculator_name}
-                    </td>
-                    <td>{element.template_name}</td>
-                    <td>{element.createdAt}</td>
-                    <td style={{ width: 145, paddingLeft: 30 }}>
-                      {element.visits}
-                    </td>
-                    <td
-                      style={{
-                        width: 110,
-                      }}
-                    >
-                      {element.unique_ip}
-                    </td>
-                    <td>{element.actions}</td>
-                  </tr>
-                ))}
-              </tbody>
-            )}
-          </Table>
-        </ScrollArea>
-        <div>
-          <Paginate
-            refetch={refetch}
-            page={sortedData ? Math.ceil(sortedData?.length / limit) : 10}
-            limit={limit}
-            totalResults={sortedData?.length}
-            setLimit={setLimit}
-            activePage={activePage}
-            setPage={setPage}
-            firstIndex={indexOfFirstPost}
-            lastIndex={indexOfLastPost}
-          />
+              <thead
+                className={cx(classes.header, { [classes.scrolled]: scrolled })}
+                style={{ zIndex: 50 }}
+              >
+                <tr>
+                  <Th
+                    sorted={sortBy === "username"}
+                    reversed={reverseSortDirection}
+                    onSort={() => setSorting("username")}
+                    style={{ width: 300 }}
+                  >
+                    User Name
+                  </Th>
+                  <Th
+                    sorted={sortBy === "calculator_name"}
+                    reversed={reverseSortDirection}
+                    onSort={() => setSorting("calculator_name")}
+                    style={{ width: 250 }}
+                  >
+                    Calculator Name
+                  </Th>
+                  <Th
+                    sorted={sortBy === "template_name"}
+                    reversed={reverseSortDirection}
+                    onSort={() => setSorting("template_name")}
+                    style={{ width: 250 }}
+                  >
+                    Template Name
+                  </Th>
+                  <Th
+                    sorted={sortBy === "createdAt"}
+                    reversed={reverseSortDirection}
+                    onSort={() => setSorting("createdAt")}
+                    style={{ width: 250 }}
+                  >
+                    Created Date
+                  </Th>
+                  <Th
+                    sorted={sortBy === "visits"}
+                    reversed={reverseSortDirection}
+                    onSort={() => setSorting("visits")}
+                    style={{ width: 110 }}
+                  >
+                    Visits
+                  </Th>
+                  <Th
+                    sorted={sortBy === "unique_ip"}
+                    reversed={reverseSortDirection}
+                    onSort={() => setSorting("unique_ip")}
+                    style={{ width: 130 }}
+                  >
+                    Unique
+                  </Th>
+                  <th></th>
+                </tr>
+              </thead>
+              {isLoading ? (
+                <SkeletonLoader />
+              ) : (
+                <tbody>
+                  {companyUsers?.map((element: any) => (
+                    <tr key={shortUUID.generate()} style={{ height: 20 }}>
+                      <td style={{ width: 10 }}>{element.username}</td>
+                      <td
+                        style={{
+                          cursor: "pointer",
+                          width: 140,
+                          paddingLeft: 30,
+                        }}
+                      >
+                        {element.calculator_name}
+                      </td>
+                      <td>{element.template_name}</td>
+                      <td>{element.createdAt}</td>
+                      <td style={{ width: 145, paddingLeft: 30 }}>
+                        {element.visits}
+                      </td>
+                      <td
+                        style={{
+                          width: 110,
+                        }}
+                      >
+                        {element.unique_ip}
+                      </td>
+                      <td>{element.actions}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              )}
+            </Table>
+          </ScrollArea>
+          <div>
+            <Paginate
+              refetch={refetch}
+              page={sortedData ? Math.ceil(sortedData?.length / limit) : 10}
+              limit={limit}
+              totalResults={sortedData?.length}
+              setLimit={setLimit}
+              activePage={activePage}
+              setPage={setPage}
+              firstIndex={indexOfFirstPost}
+              lastIndex={indexOfLastPost}
+            />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+
+  if (isError) {
+    return <FourOhFour />;
+  }
+
+  return <></>;
 };
 
-export default CompanyUserTable
+export default CompanyUserTable;
