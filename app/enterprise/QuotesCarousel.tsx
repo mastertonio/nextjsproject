b315550dcb5f706a7d381acb9a6b44cf920791f8
jason-narrasoft <jason.ronda.ns@gmail.com>
2022-12-21 07:13:27 +0800

    remove dummy data for carousel

--- app/enterprise/QuotesCarousel.tsx
+++ app/enterprise/QuotesCarousel.tsx
@@ -7,30 +7,6 @@ import { useRef } from 'react';
 import { QuoteCarouselProps } from './Projection';
 
 
-const quotes = [
-    {
-        type: "quote",
-        quote: {
-            text: "We love the tool! It is changing the conversation we have with our existing customers.",
-            author: "David Verhaag, Vice President Customer Success",
-        },
-    },
-    {
-        type: "quote",
-        quote: {
-            text: "Hey Michael, I created an ROI for my first client yesterday and we closed today. The ROI Shop made getting approval from the CFO a piece of cake and resulted in my first sale!",
-            author: "Natalie Grant - Sales Rep",
-        },
-    },
-    {
-        type: "quote",
-        quote: {
-            text: "IT spending is on the decline - It will be more important than ever to arm your <q>Champion</q>> with the tools necessary to sell the project internally.",
-            author: "Gartner",
-        },
-    },
-]
-
 const QuotesCarousel: React.FC<QuoteCarouselProps> = ({ type, position, elements }) => {
     const autoplay = useRef(Autoplay({ delay: 4000 }));
     const slides = elements.map((quote) => (
@@ -43,7 +19,8 @@ const QuotesCarousel: React.FC<QuoteCarouselProps> = ({ type, position, elements
 
     return (
         <Carousel
-            style={{ backgroundColor: "#fff" }}
+            mb={8}
+            style={{ }}
             loop
             withControls={false}
             plugins={[autoplay.current]}
