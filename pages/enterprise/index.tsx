dc44ddaaa7dddd59762bd84b5a7b9051b08375f3
jason-narrasoft <jason.ronda.ns@gmail.com>
2022-12-21 07:13:55 +0800

    enterprise page changes

--- pages/enterprise/index.tsx
+++ pages/enterprise/index.tsx
@@ -4,11 +4,9 @@ import Projection from "@app/enterprise/Projection";
 import { AppShell, Navbar, Header, Grid, SimpleGrid } from "@mantine/core";
 import { contentData, finalData } from "@app/enterprise/constants/content";
 import SliderCard from "@app/core/components/card";
+import InputVariable, { iElemsProp } from "@app/core/components/input";
 
 const Enterprise = () => {
-  const getContentType = (content: any) => {
-
-  }
 
   return (
     <AppShell
@@ -21,36 +19,55 @@ const Enterprise = () => {
             theme.colorScheme === "dark"
               ? theme.colors.dark[8]
               : theme.colors.gray[0],
-          margin: 0
+          margin: 0,
         },
       })}
       style={{ margin: 0, padding: 0 }}
     >
       <>
-        {finalData ? finalData.sections.map((section) => (
-          <div style={{ width: "100%", color: "#676a6c" }} key={section.id}>
-            <Projection
-              title={section.headers.title.mainTitle.text}
-              description={section.headers.title.description}
-              subTitle={section.headers.title.subTitle.text}
-              content={section.headers.title.content}
-              length={section.headers.title.content.elements.length}
-              quotes={section.headers.title.quotes}
-            />
+        {finalData
+          ? finalData.sections.map((section) => (
+              <div style={{ width: "100%", color: "#676a6c" }} key={section.id}>
+                <Projection
+                  title={section.headers.title.mainTitle.text}
+                  description={section.headers.title.description}
+                  subTitle={section.headers.title.subTitle.text}
+                  content={section.headers.title.content}
+                  length={section.headers.title.content.elements.length}
+                  quotes={section.headers.title.quotes}
+                />
 
-            {/* filter Gray Contents here */}
-            {section.GrayContent.type == "sliders" ?
-              (<SimpleGrid cols={3} p={10} pr={20} style={{ backgroundColor: "#e9ecef" }}>
-                {contentData.sections.sliders.elements.map((element) => <SliderCard key={element.id} label={element.title} money={element.money} progress={element.value} />)}
-              </SimpleGrid>)
-              : section.GrayContent.type == "" ?
-                (<SimpleGrid cols={3} p={10} pr={20} style={{ backgroundColor: "#e9ecef" }}>
-                 <div style={{ height: 100}}></div>
-                </SimpleGrid>)
-                : ""
-            }
-
-          </div>)) : ""}
+                <div>
+                  {/* filter Gray Contents here */}
+                  {section.GrayContent.type == "sliders" ? (
+                    <SimpleGrid
+                      cols={3}
+                      p={10}
+                      pr={20}
+                      style={{ backgroundColor: "#e9ecef" }}
+                    >
+                      {contentData.sections.sliders.elements.map((element) => (
+                        <SliderCard
+                          key={element.id}
+                          label={element.title}
+                          money={element.money}
+                          progress={element.value}
+                        />
+                      ))}
+                    </SimpleGrid>
+                  ) : section.GrayContent.type == "variables" ? (
+                    <div style={{ backgroundColor: "#e9ecef" }}>
+                        <InputVariable id={section.id} type={section.GrayContent.type} />
+                    </div>
+                  ) : (
+                    <div style={{ backgroundColor: "#e9ecef" }}>
+                      <br></br>
+                    </div>
+                  )}
+                </div>
+              </div>
+            ))
+          : ""}
         {/* <div style={{ width: "100%", color: "#676a6c" }}>
           <Projection
             title={contentData.sections.headers.title.mainTitle.text}
@@ -63,7 +80,6 @@ const Enterprise = () => {
           </SimpleGrid>
         </div> */}
       </>
-
     </AppShell>
   );
 };
