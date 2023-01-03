716949f5a191e217a8f17c35c93deac5bbf78932
jason-narrasoft <jason.ronda.ns@gmail.com>
2023-01-03 22:36:53 +0800

    floating card summary

--- app/enterprise/components/FloatingCard.tsx
+++ app/enterprise/components/FloatingCard.tsx
@@ -1,20 +1,79 @@
-import { Card, Image, Text, Badge, Button, Group } from '@mantine/core';
+import { Card, Image, Text, Badge, Button, Group, Divider, Grid, Slider } from '@mantine/core';
 
 const FloatingCard: React.FC = () => {
   return (
-    <Card shadow="sm" p="lg" radius="md" withBorder>
-      <Card.Section p={10} >
-        <Text>Improve Win Rate</Text>
-      </Card.Section>
+    <div style={{ width: '100%', position: 'sticky', top: 70, borderRadius: 10 }}>
+      {/* w={'100%'} pos="sticky" top={70}  */}
+      <Card mb={10} shadow="sm" p="lg" radius="md" withBorder>
+        <Card.Section pt={20} pl={20} >
+          <Text fz={25} >Improve Win Rate</Text>
+        </Card.Section>
 
-      <Text size="sm" color="dimmed">
-        This will be the summary floater which will float only on its corresponding section
-      </Text>
+        <Divider my="xs" size="xs" variant="solid" />
 
-      <Button variant="light" color="blue" fullWidth mt="md" radius="md">
-        Slider or any content here
-      </Button>
-    </Card>
+        <Grid p={10}>
+          <Text size="sm" color="dimmed">
+            Year 1:
+          </Text>
+          <Text ml={'auto'} size="sm" color="dimmed">
+            $0
+          </Text>
+        </Grid>
+
+        <Divider my="xs" size="xs" variant="solid" />
+
+        <Grid p={10}>
+          <Text size="sm" color="dimmed">
+            Year 2:
+          </Text>
+          <Text ml={'auto'} size="sm" color="dimmed">
+            $0
+          </Text>
+        </Grid>
+
+        <Divider my="xs" size="xs" variant="solid" />
+
+        <Grid p={10}>
+          <Text size="sm" color="dimmed">
+            Improve Win Rate Total:
+          </Text>
+          <Text ml={'auto'} size="sm" color="dimmed">
+            $0
+          </Text>
+        </Grid>
+
+        <Slider
+          color="teal"
+          size="xl"
+          radius="md"
+          defaultValue={70}
+          mb={30}
+          marks={[
+            { value: 20, label: '20%' },
+            { value: 50, label: '50%' },
+            { value: 90, label: '90%' },
+          ]}
+        />
+      </Card>
+      <Card shadow="sm" p="lg" radius="md" withBorder>
+        <Grid p={10}>
+          <Text size="sm" color="dimmed">
+            Implementation Period:
+          </Text>
+          <Text ml={'auto'} size="sm" color="dimmed">
+            n months
+          </Text>
+        </Grid>
+        <Slider
+          color="teal"
+          size="xl"
+          radius="md"
+          mb={5}
+          defaultValue={30}
+        />
+      </Card>
+    </div>
+
   );
 }
 
