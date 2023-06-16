f9cc2acc86d81cc63631606f240ecc425e1a6eee
Jason Ronda <jason.ronda.ns@gmail.com>
2023-06-16 18:17:02 +0800

    ranking changes

--- app/dashboard/components/RoiRanking.tsx
+++ app/dashboard/components/RoiRanking.tsx
@@ -38,25 +38,27 @@ const RoiRanking: React.FC<UserDataProp> = ({ tokens, user }) => {
   //   .sort((a: { totalROIS: number }, b: { totalROIS: number }) => b.totalROIS - a.totalROIS)
   //   .slice(0, 5);
 
-
-
-  const me = data?.data.find((elem: { _id: string }) => elem._id === user.id);
   // .concat(others.slice(3, 4))
   // const rank4 = elements.slice(0,4)
   // const me = elements.filter((elem: { _id: string; }) => user.id == elem._id)
   // const newRanking = rank4.push(me)
   // console.log(newRanking)
-  const [rank, setRank] = useState()
+
+
+  const me = data?.data.find((elem: { _id: string }) => elem._id === user.id);
+  const rank = data?.data?.sort((a: { totalROIS: number; }, b: { totalROIS: number; }) => b.totalROIS - a.totalROIS).slice(0,4)
+  
   const rows = data?.data?.sort((a: { totalROIS: number; }, b: { totalROIS: number; }) => b.totalROIS - a.totalROIS).map((element: { _id: Key | null | undefined; name: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | ReactFragment | ReactPortal | null | undefined; totalROIS: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | ReactFragment | ReactPortal | null | undefined; }, index: number) => (
     <tr key={element._id}>
-      <td><span className="bg-[#1ab394] pt-[3px] pb-[3px] pl-[8px] pr-[8px] m-[15px] text-white rounded-[4px]">{index + 1}</span></td>
+      <td><span className={`${element._id== me._id ? "bg-yellow-500" : "bg-[#1ab394]"} pt-[3px] pb-[3px] pl-[8px] pr-[8px] m-[15px] text-white rounded-[4px]`}>{index + 1}</span></td>
       <td>{element.name}</td>
       <td>{element.totalROIS}</td>
     </tr>
-  )).slice(0,4);
+  ));
 
+  const realRows = rank?.includes(me) ? rows?.slice(0,5) : rows?.slice(0,4) 
   
-  const indexMe = rows?.indexOf(me)+1
+  const indexMe = rows?.indexOf(me)
 
   // useEffect(()=> {
   //   setRank(rows)
@@ -73,8 +75,8 @@ const RoiRanking: React.FC<UserDataProp> = ({ tokens, user }) => {
           </tr>
         </thead>
         <tbody>
-          {rows}
-          {me?.totalROIS > 0 ? (<tr>
+          {realRows}
+          {me?.totalROIS && !(rank.includes(me) > 0) ? (<tr>
             <td><span className="bg-yellow-500 pt-[3px] pb-[3px] pl-[8px] pr-[8px] m-[15px] text-white rounded-[4px]">{indexMe+1}</span></td>
             <td>{me?.name}</td>
             <td>{me?.totalROIS}</td>
